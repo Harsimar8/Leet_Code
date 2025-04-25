7f0cb7ad1988a0bd0a3452f4b30f5e6c4aee10d4
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-04-26 01:50:14 +0530

    Time: 2 ms (61.76%), Space: 44.3 MB (64.11%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,33 +14,31 @@
  * }
  */
 class Solution {
-    private HashMap<Integer , Integer> indexMap = new HashMap<>();
+    private HashMap<Integer , Integer> indexMap;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-         if(preorder.length != inorder.length){
-            throw new IllegalArgumentException("Length of both are not same");
+        if(preorder.length != inorder.length){
+            throw new IllegalArgumentException("Length of both are not equal");
         }
         indexMap = new HashMap<>();
         for(int i =0; i<inorder.length; i++){
-            indexMap.put(inorder[i] , i);
+            indexMap.put(inorder[i],i);
         }
-        return  constructTreeHelper(preorder,inorder , 0,preorder.length-1, 0 , inorder.length -1);
+        return constructTreeHelper(preorder ,inorder , 0, preorder.length -1, 0 ,inorder.length-1);
     }
-
-    private  TreeNode constructTreeHelper(int[] preorder , int[] inorder , int preStart, int preEnd, int inStart , int inEnd){
-        if(preStart> preEnd || inStart> inEnd){
+    private TreeNode constructTreeHelper(int[] preorder , int[] inorder , int preStart , int preEnd , int inStart , int inEnd){
+        if(preStart > preEnd || inStart > inEnd){
             return null;
         }
         int rootData = preorder[preStart];
+
         TreeNode root = new TreeNode(rootData);
         int rootIndex = indexMap.get(rootData);
+        
+        int  leftTreeSize = rootIndex - inStart;
 
-        int leftTreeSize = rootIndex - inStart;
-        root.left = constructTreeHelper(preorder,inorder, preStart+1, preStart+ leftTreeSize, inStart, rootIndex-1);
-        root.right = constructTreeHelper(preorder,inorder, preStart+ leftTreeSize + 1, preEnd, rootIndex+ 1, inEnd);
-        return  root;
-
-
+        root.left = constructTreeHelper(preorder , inorder , preStart +1, preStart + leftTreeSize , inStart, rootIndex -1);
+        root.right = constructTreeHelper(preorder , inorder , preStart + leftTreeSize +1, preEnd, rootIndex+1, inEnd);
+        return root;
 
     }
-
 }
