090744788692acbc74f2ce4c0512c36a999f25ea
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-06-06 05:57:32 +0530

    Time: 2 ms (97.27%), Space: 41.9 MB (47.99%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,29 +4,27 @@ class Solution {
         Stack<Character> st = new Stack<>();
         for(int i =0; i<n; i++){
             char ch = s.charAt(i);
-        if(ch == '[' ||  ch == '{' || ch == '('){
-            st.push(ch);
-        }
-        else if(st.isEmpty()){
-            return false;
-        }
-        else{
-            char c = st.peek();
-            if(ch == ']' && st.peek() == '['){
-                st.pop();
-            }
-            else if(ch == '}' && st.peek() == '{'){
-                st.pop();
-            }
-            else if(ch == ')' && st.peek() == '('){
-                st.pop();
-            }
-            else{
+            if(ch == '[' || ch == '{' || ch == '('){
                 st.push(ch);
             }
-
-        }
-         
+            else if(st.isEmpty()){
+                return false;
+            }
+            else{
+                char c = st.peek();
+                if(ch == ']' && st.peek() == '['){
+                    st.pop();
+                }
+                else if(ch == '}' && st.peek() == '{'){
+                    st.pop();
+                }
+                else if(ch == ')' && st.peek() == '('){
+                    st.pop();
+                }
+                else{
+                    st.push(ch);
+                }
+            }
         }
         return (st.size() == 0);
     }
