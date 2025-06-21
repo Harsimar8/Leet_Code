a37eff2d10d237b9e037c15076bca825e472703e
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-06-22 02:35:08 +0530

    Time: 0 ms (100%), Space: 43.1 MB (81.03%) - LeetHub

--- 1185-find-in-mountain-array/1185-find-in-mountain-array.java
+++ 1185-find-in-mountain-array/1185-find-in-mountain-array.java
@@ -9,60 +9,57 @@
  
 class Solution {
     public int findInMountainArray(int target, MountainArray mountainArr) {
-        int start =0;
-        int mid = -1;
+        int start = 0;
         int n = mountainArr.length();
         int end = n-1;
+        int mid =-1;
         while(start <= end){
-             mid = start + (end - start)/2;
+            mid = start + (end - start)/2;
             int curr = mountainArr.get(mid);
-            if(mid!=0 && mid!=n-1 &&  curr > mountainArr.get(mid +1) && 
-             curr > mountainArr.get(mid-1)){
+            if(mid!=0 && mid!= n-1 && curr > mountainArr.get(mid+1) && 
+                         curr > mountainArr.get(mid-1)){
                 if(curr == target){
                     return mid;
                 }
-                break;
-             }
-             else if(curr < mountainArr.get(mid+1)){
+                 break;
+                }
+            else if(curr < mountainArr.get(mid+1)){
                 start = mid +1;
-             }
-             else{
+            }             
+            else{
                 end = mid -1;
-             }
+            }
         }
         start =0;
         end = mid -1;
         while(start <= end){
-            int m = start + (end - start)/2;
+            int m  = start + (end - start)/2;
             int cur = mountainArr.get(m);
             if(cur == target){
                 return m;
             }
-             else if(cur > target){
-                end = m -1;
-             }
-             else{
+            else if(cur < target){
                 start = m +1;
-             }
+            }
+            else{
+                end = m -1;
+            }
         }
-        start = mid +1;
+        start=  mid +1;
         end = n-1;
-         while(start <= end){
-            int m = start + (end - start)/2;
+        while(start <= end){
+            int m  = start + (end - start)/2;
             int cur = mountainArr.get(m);
-                if(cur == target){
-                    return m;
-                }
-              
-             else if(cur > target){
+            if(cur == target){
+                return m;
+            }
+            else if(cur < target){
+                end = m-1;
+            }
+            else{
                 start = m +1;
-             }
-             else{
-                end = m -1;
-             }
+            }
         }
-        return -1;
-        
-
+     return -1;   
     }
 }
