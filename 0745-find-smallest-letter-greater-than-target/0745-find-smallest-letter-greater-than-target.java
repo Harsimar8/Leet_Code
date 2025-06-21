b9519457e67877cba50e70634d580e993ded5808
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-06-22 02:42:50 +0530

    Time: 0 ms (100%), Space: 44.7 MB (27.29%) - LeetHub

--- 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.java
+++ 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.java
@@ -1,16 +1,17 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        char res = binarySearchCeil(letters, target );
+        char res = binaryS(letters, target);
         return res;
     }
-    public static char binarySearchCeil(char arr[], char target){
+    public  static char binaryS(char[] arr, char target){
         int start =0;
-        int end = arr.length -1;
-        char ans ='$';
-        while(start <=end){
-            int mid = start + (end - start) /2;
+        int n = arr.length;
+        int end = n-1;
+        char ans = '$';
+        while(start <= end){
+            int mid = start + (end - start)/2;
             if(arr[mid]<=target){
-                start = mid + 1;
+                start = mid+1;
             }
             else{
                 end = mid -1;
