ef945b2576388339fb280d5762a62151585fe10d
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-06-22 08:56:23 +0530

    Time: 0 ms (100%), Space: 42.2 MB (52.9%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -18,8 +18,8 @@ class Solution {
                 }
             }
             else{
-                if(target > nums[mid] && target <= nums[end]){
-                    start = mid +1;
+                if(target >= nums[mid] && target <= nums[end]){
+                    start  = mid +1;
                 }
                 else{
                     end = mid -1;
