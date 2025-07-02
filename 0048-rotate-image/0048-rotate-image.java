d37e13125f85fc6cebe7b8db5a46450bb8a64fae
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-07-02 22:14:05 +0530

    Time: 0 ms (100%), Space: 42.4 MB (25.52%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,29 +1,25 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int[][] transposed = transpose(matrix); 
-        int n = transposed.length;
+        transpose(matrix);
+        int n = matrix.length;
         for(int i =0; i<n; i++){
             for(int j =0; j<n/2; j++){
-                int temp = transposed[i][j];
-                transposed[i][j] =  transposed[i][n-j-1]; 
-                transposed[i][n-j-1] = temp;
+                int temp = matrix[i][j];
+                matrix[i][j] =  matrix[i][n-j-1]; 
+                matrix[i][n-j-1] = temp;
             }
         }
-        for(int i =0; i<n; i++){
-            for(int j =0; j<n; j++){
-                matrix[i][j] = transposed[i][j];
-            }
-        }
+        
     }
-    public int[][] transpose(int[][] arr){
-        int r= arr.length;
-        int c = arr[0].length;
-        int[][] naa = new int[c][r];
-        for(int i =0; i<c; i++){
-            for(int j =0; j<r; j++){
-                naa[i][j] = arr[j][i];
+    public void transpose(int[][] arr){
+        int n = arr.length;
+        for(int i =0; i<n; i++){
+            for(int j =0; j<i; j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
             }
         }
-        return naa; 
+
     }
 }
