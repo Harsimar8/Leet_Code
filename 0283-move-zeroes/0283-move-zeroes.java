db371d1d093d7a66c9369d2775f96ac648659ac8
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-07-10 22:30:31 +0530

    Time: 2 ms (80.46%), Space: 46.1 MB (62.35%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int[] arr= new int[n];
+        
+        int j =0;
+        for(int i =0; i<n; i++){
+            if(nums[i] != 0){
+                arr[j++] = nums[i];
+            }
+        }
+        int l =0;
+        for(int k=0; k<arr.length; k++){
+            nums[k] = arr[l];
+            l++; 
+        }
+        
+
+    }
+}
