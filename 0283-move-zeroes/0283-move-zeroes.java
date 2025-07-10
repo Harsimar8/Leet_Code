dbd1ea3c0f5d8872ba4b08aff2e6a171ae5aa100
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-07-10 22:53:39 +0530

    Time: 58 ms (5.83%), Space: 46.5 MB (7.67%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,20 +1,33 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int n = nums.length;
-        int[] arr= new int[n];
-        
-        int j =0;
+    public void moveZeroes(int[] arr) {
+        int n = arr.length;
         for(int i =0; i<n; i++){
-            if(nums[i] != 0){
-                arr[j++] = nums[i];
+            if(arr[i] != 0){
+                arr[i] = arr[i];
             }
         }
-        int l =0;
-        for(int k=0; k<arr.length; k++){
-            nums[k] = arr[l++];
-           
+        int findZ =0;
+        for(int i=0; i<n; i++){
+            if(arr[i] == 0 ){
+                findZ =i;
+                break;
+            }
         }
+        if(arr[findZ] == 0){
+        for(int i = findZ; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                if(arr[j] != 0){
+                    swap(arr, i, j);
+                    break;
+                }
+            }
+        }
+        }
+    }
+    public void swap(int arr[] , int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
         
-
     }
 }
