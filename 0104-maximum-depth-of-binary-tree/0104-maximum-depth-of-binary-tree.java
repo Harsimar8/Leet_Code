1ecff77d9088831cf1d0ae425d060abf5165eb34
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-07-16 11:36:39 +0530

    Time: 0 ms (100%), Space: 43.4 MB (5.67%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -18,10 +18,10 @@ class Solution {
         if(root==null){
             return 0;
         }
-        else{
+        
         int left =maxDepth(root.left);
         int right = maxDepth(root.right);
         return 1 + Math.max(left,right);
-        }
+        
     }
 }
