25870558e04fa9968c13b5a5cfe116e91f749465
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-07-17 11:07:03 +0530

    Time: 2 ms (73.72%), Space: 53.6 MB (14.13%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -15,6 +15,19 @@ class Solution {
                 cnt--;
             }
         }
-        return ele;
+        int cnt1 =0;
+        for(int i =0; i<n;i++){
+            if(ele == nums[i]){
+                cnt1++;
+            }
+            
+        }
+        if(cnt1 > n/2){
+                return ele;
+            }
+            else{
+                return -1;
+            }
+        
     }
 }
