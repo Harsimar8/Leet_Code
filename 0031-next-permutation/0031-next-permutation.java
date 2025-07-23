d312aa7f39acdc1ac647cd9e797c4c02a8e20379
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-07-23 10:41:55 +0530

    Time: 0 ms (100%), Space: 43.6 MB (5.94%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -10,7 +10,7 @@ class Solution {
             }
         }
         if(ind == -1){
-            reverse(nums,0,n-1);
+            reverse(nums);
             
         }
         if(ind != -1){    
@@ -21,12 +21,13 @@ class Solution {
             }
             
         }
-        reverse(nums, ind+1, n-1);
+        sort(nums, ind+1);
         }
     }
-    void reverse(int nums[],int left, int right){
+    void reverse(int nums[]){
         int n = nums.length;
-        
+        int left =0;
+        int right = n-1;
         while(left <= right){
             int temp = nums[left];
             nums[left] = nums[right];
@@ -35,21 +36,21 @@ class Solution {
             right--;
         }
     }
-        // void sort(int[] nums, int start){
-        //     int n = nums.length;
-        // for(int i =start;i<n-1;i++){
-        //     boolean swap =false;
-        //     for(int j =start; j<n; j++){
-        //         if(nums[j] > nums[j+1]){
-        //             swap(nums, j, j+1);
-        //             swap = true;
-        //         }
-        //     }
-        //     if(!swap){
-        //         break;
-        //     }
-        // }
-        // }
+        void sort(int[] nums, int start){
+            int n = nums.length;
+        for(int i =start;i<n-1;i++){
+            boolean swap =false;
+            for(int j =start; j<n - (i - start) - 1; j++){
+                if(nums[j] > nums[j+1]){
+                    swap(nums, j, j+1);
+                    swap = true;
+                }
+            }
+            if(!swap){
+                break;
+            }
+        }
+        }
         void swap(int nums[] , int i , int j){
             int temp = nums[i];
             nums[i] = nums[j];
