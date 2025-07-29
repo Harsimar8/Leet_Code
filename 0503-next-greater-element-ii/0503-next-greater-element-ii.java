10d3ff41cbc23d4c012c6881df8dc09f2d76ece1
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-07-29 12:07:00 +0530

    Time: 30 ms (13.24%), Space: 45.6 MB (98.57%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,29 +1,29 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-       int n = nums.length;
-       int[] ans = new int[n];
-       for(int i =0; i<n; i++){
-        int elem = nums[i];
-        boolean flag = false;
-        int res= -1;
-        for(int j = i+1; j<n; j++){
-            if(nums[j] > elem){
-                res = nums[j];
-                flag = true;
-                break;
-            }
-            
-        }
-        if(!flag){
-                for(int k =0 ; k<= i-1; k++){
-                    if(nums[k] > elem){
-                    res = nums[k];
+        int n = nums.length;
+        int[] res = new int[n];
+        for(int i =0; i<n; i++){
+            int elem = nums[i];
+            int ans =-1;
+            boolean flag = false;
+            for(int j =i+1; j<n; j++){
+                if(nums[j] > elem){
+                    ans = nums[j];
+                    flag =true;
                     break;
-                    }   
+                }
+            }
+            if(!flag){
+                for(int k =0;k<i; k++){
+                    if(nums[k] > elem){
+                        ans = nums[k];
+                        break;
+                    }               
+                    
+                }
+            }
+            res[i] = ans;
         }
-        }
-        ans[i] = res;
-       }
-       return ans; 
+        return res;
     }
 }
