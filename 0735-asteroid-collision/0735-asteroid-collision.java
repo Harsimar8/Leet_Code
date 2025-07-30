724a51074e26e1a64e99421c5327207cc3a10c2e
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-07-30 17:26:01 +0530

    Time: 8 ms (15.84%), Space: 45.6 MB (34.1%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,44 +1,55 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> st = new Stack<>();
-        int n = asteroids.length;
-
-        for (int i = 0; i < n; i++) {
-            if (asteroids[i] > 0) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int n =asteroids.length;
+        for(int i =0; i<n; i++){
+            if(asteroids[i] > 0){
+                
                 st.push(asteroids[i]);
-            } else {
-                while (!st.isEmpty() && st.peek() > 0 && st.peek() < Math.abs(asteroids[i])) {
-                    st.pop(); // smaller positive asteroid destroyed
-                }
-
-                if (!st.isEmpty() && st.peek() > 0 && st.peek() == Math.abs(asteroids[i])) {
-                    st.pop(); // both destroyed
-                } else if (st.isEmpty() || st.peek() < 0) {
-                    st.push(asteroids[i]); // no collision or top is negative
-                }
+                
             }
+            else{
+                
+                while(!st.isEmpty() && st.peek() > 0 &&  Math.abs(asteroids[i]) > st.peek() ){
+                    
+                    st.pop();
+                }
+                
+                if(!st.isEmpty() && st.peek() > 0 && Math.abs(asteroids[i]) == st.peek() ){
+                    st.pop();
+                }
+                else if(st.isEmpty() || st.peek()  <0){
+                    st.push(asteroids[i]);
+                }
+                
+                
+                
+                
+            }
+            
         }
-
         int[] res = new int[st.size()];
-        int i = 0;
-        while (!st.isEmpty()) {
+        int i =0;
+        while(!st.isEmpty()){
             res[i++] = st.pop();
         }
-
+        
         reverse(res);
         return res;
     }
 
-    public void reverse(int[] res) {
+    public void reverse(int[] res){
         int n = res.length;
-        int left = 0;
-        int right = n - 1;
-        while (left <= right) {
+        int left =0;
+        int right = n-1;
+        while(left <= right){
             int temp = res[left];
             res[left] = res[right];
             res[right] = temp;
             left++;
             right--;
         }
+
     }
 }
