730e2d16a9a5daa5e3293a157504034e6c2d18ff
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-07-31 12:24:38 +0530

    Time: 6 ms (64.2%), Space: 45.4 MB (73.57%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,48 +1,34 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> st = new Stack<>();
-       
-       for (int curr : asteroids) {
-            boolean destroyed = false;
 
-            while (!st.isEmpty() && curr < 0 && st.peek() > 0) {
-                if (st.peek() < -curr) {
-                    st.pop(); // Right asteroid is smaller — destroy it
-                } else if (st.peek() == -curr) {
-                    st.pop(); // Both destroy each other
-                    destroyed = true;
+        Stack<Integer> st =new Stack<>();
+        for(int curr : asteroids){
+            boolean destroy = false;
+            while(!st.isEmpty() && curr < 0 && st.peek() > 0){
+                if(st.peek() < Math.abs(curr)){
+                    
+                    st.pop();
+                    
+                }
+                else if(st.peek() == Math.abs(curr)){
+                    destroy = true;
+                    st.pop();
                     break;
-                } else {
-                    destroyed = true; // Current asteroid is smaller — destroy it
+                }
+                else{
+                    destroy = true;
                     break;
                 }
             }
-
-            if (!destroyed) {
+            if(!destroy){
                 st.push(curr);
             }
-       }
+        }
         int[] res = new int[st.size()];
-        int i =0;
+        int i =st.size() -1;
         while(!st.isEmpty()){
-            res[i++] = st.pop();
+            res[i--] = st.pop();
         }
-        
-        reverse(res);
         return res;
-    }
-
-    public void reverse(int[] res){
-        int n = res.length;
-        int left =0;
-        int right = n-1;
-        while(left <= right){
-            int temp = res[left];
-            res[left] = res[right];
-            res[right] = temp;
-            left++;
-            right--;
-        }
-
     }
 }
