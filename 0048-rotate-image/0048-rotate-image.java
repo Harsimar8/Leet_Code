3927fa6a7e569d5219113ffd9ef5eebe1772ee93
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-08-04 02:06:23 +0530

    Time: 0 ms (100%), Space: 42.3 MB (59%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,25 +1,34 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        transpose(matrix);
-        int n = matrix.length;
-        for(int i =0; i<n; i++){
-            for(int j =0; j<n/2; j++){
-                int temp = matrix[i][j];
-                matrix[i][j] =  matrix[i][n-j-1]; 
-                matrix[i][n-j-1] = temp;
+        int a = matrix.length;
+        int b = matrix[0].length;
+        int[][] ans = new int[a][b];
+        for(int i =0; i<a;i++){
+            for(int j =0; j<b;j++){
+                ans[i][j] = matrix[j][i];
             }
         }
-        
-    }
-    public void transpose(int[][] arr){
-        int n = arr.length;
-        for(int i =0; i<n; i++){
-            for(int j =0; j<i; j++){
-                int temp = arr[i][j];
-                arr[i][j] = arr[j][i];
-                arr[j][i] = temp;
+        for(int i =0; i<a; i++){
+            swap(ans[i]);
+        }
+        for(int i =0; i<a; i++){
+            for(int j =0; j<b; j++){
+                matrix[i][j] = ans[i][j];
             }
         }
 
     }
+    public void swap(int[] arr){
+        int n = arr.length;
+        int left = 0;
+        int right = n-1;
+        while(left<=right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
 }
