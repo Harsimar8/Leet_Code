de48d2a54d6daf38494cb8f1a284b1c6fb5c292d
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-08-08 19:28:16 +0530

    Time: 64 ms (63.77%), Space: 50.9 MB (69.31%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
@@ -1,38 +1,48 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int MOD = 1_000_000_007;
         int n = arr.length;
-        Stack<Integer> stack = new Stack<>();
-        int[] prev = new int[n];
-        int[] next = new int[n];
-
-        // Previous Less Element
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
-                stack.pop();
+        
+        
+    
+   
+        int[] prefix = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i =0; i<n;i++){
+            while(!st.isEmpty() && arr[i] <= arr[st.peek()]){
+                st.pop();
             }
-            prev[i] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(i);
+            prefix[i] = (st.isEmpty()) ? -1 : st.peek();
+            st.push(i);
+            
         }
+        
 
-        stack.clear();
-
-        // Next Less Element
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
-                stack.pop();
+    
+        int[] suffix = new int[n];
+        Stack<Integer> st1 = new Stack<>();
+        for(int i =n-1; i>=0;i--){
+            while(!st1.isEmpty() && arr[i] < arr[st1.peek()]){
+                st1.pop();
             }
-            next[i] = stack.isEmpty() ? n : stack.peek();
-            stack.push(i);
+            suffix[i] = (st1.isEmpty()) ? n : st1.peek();
+            st1.push(i);
+            
         }
 
-        // Calculate result
-        long result = 0;
-        for (int i = 0; i < n; i++) {
-            long count = (i - prev[i]) * (next[i] - i);
-            result = (result + (arr[i] * count) % MOD) % MOD;
+        long sum =0;
+        int MOD = (int)1e9 +7;
+        for(int i =0; i<n;i++){
+            long pp = i-prefix[i];
+            long rr = suffix[i] -i;
+            sum  =(sum + (long) arr[i] * pp % MOD * rr % MOD)%MOD;
+            if(sum < 0){
+                sum += MOD;
+            }
         }
+        
+        return (int)sum;
+        
 
-        return (int) result;
     }
 }
+
