6c89cf331087344201c3788c0b521d637253e39d
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-08-12 00:10:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
@@ -1,48 +1,34 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int n = arr.length;
-        
-        
-    
-   
+        int n  = arr.length;
         int[] prefix = new int[n];
         Stack<Integer> st = new Stack<>();
-        for(int i =0; i<n;i++){
-            while(!st.isEmpty() && arr[i] <= arr[st.peek()]){
+        Stack<Integer> st1 = new Stack<>();
+
+
+        for(int i =0; i<n; i++){
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
                 st.pop();
             }
             prefix[i] = (st.isEmpty()) ? -1 : st.peek();
             st.push(i);
-            
         }
-        
-
-    
-        int[] suffix = new int[n];
-        Stack<Integer> st1 = new Stack<>();
-        for(int i =n-1; i>=0;i--){
-            while(!st1.isEmpty() && arr[i] < arr[st1.peek()]){
+         int[] suffix = new int[n];
+        for(int i =n-1; i>=0; i--){
+             while(!st1.isEmpty() && arr[st1.peek()] > arr[i]){
                 st1.pop();
             }
             suffix[i] = (st1.isEmpty()) ? n : st1.peek();
             st1.push(i);
-            
         }
-
         long sum =0;
-        int MOD = (int)1e9 +7;
+        int MODE = (int)1e9 + 7;
         for(int i =0; i<n;i++){
-            long pp = i-prefix[i];
-            long rr = suffix[i] -i;
-            sum  =(sum + (long) arr[i] * pp % MOD * rr % MOD)%MOD;
-            if(sum < 0){
-                sum += MOD;
+            sum += ((long)arr[i] * ((i-prefix[i])%MODE *(suffix[i]-i)%MODE)%MODE);
+            if( sum < 0){
+                sum += MODE;
             }
         }
-        
         return (int)sum;
-        
-
     }
 }
-
