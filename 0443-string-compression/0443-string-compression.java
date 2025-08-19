60c5f68247fd58b97a7b793db3cc51233d64a511
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-08-19 15:22:47 +0530

    Time: 1 ms (99.61%), Space: 43.2 MB (99.09%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,24 +1,27 @@
 class Solution {
     public int compress(char[] chars) {
-        int anchor = 0; // Starting point of the current character sequence
-        int write = 0; // Index for writing the compressed characters
-
-        for (int read = 0; read < chars.length; read++) {
-            // If this is the last character or the next character is different
-            if (read + 1 == chars.length || chars[read + 1] != chars[read]) {
-                chars[write++] = chars[anchor]; // Write the character
-
-                // If the sequence has more than one character, write the count
-                if (read > anchor) {
-                    for (char c : Integer.toString(read - anchor + 1).toCharArray()) {
-                        chars[write++] = c;
-                    }
-                }
-
-                anchor = read + 1; // Move the anchor to the next sequence
+        int i = 0;
+        int len = chars.length;
+        int j = 0;
+        StringBuilder sb = new StringBuilder();
+        if(len == 1) return 1;
+        while(i < len){
+            int count = 1;
+            char c = chars[i];
+            while(i < len - 1 && chars[i] == chars[i+1]){
+                count++;
+                i++;
             }
+            chars[j++] = c;
+            if(count > 1){
+                sb.append(count);
+                while(sb.length() > 0){
+                    chars[j++] = sb.charAt(0);
+                    sb.deleteCharAt(0);
+                }
+            }
+            i++;
         }
-
-        return write; // Return the length of the compressed array
+        return j;
     }
 }
