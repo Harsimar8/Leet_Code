d2ca9ff341c4a1822341520485d404c77d784a39
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-08-20 18:05:25 +0530

    Time: 13 ms (67.77%), Space: 42.7 MB (48.88%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.java
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.java
@@ -11,73 +11,63 @@ class Solution {
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         HashMap<TreeNode,TreeNode> mpp = new HashMap<>();
         HashMap<TreeNode,Boolean> visited = new HashMap<>();
-        
         Queue<TreeNode> q = new LinkedList<>();
         Queue<TreeNode> q1 = new LinkedList<>();
-         //HashMap<TreeNode,TreeNode> parent = new HashMap<>();
 
-        q.add(root);
-        if(root == null){
-            return null;
-        }
+        // if(root == null){
+        //     return null;
+        // }
+        q.offer(root);
         while(!q.isEmpty()){
             int s = q.size();
             for(int i =0; i<s; i++){
-                TreeNode pair  = q.poll();
-                if(pair.left != null){
-                    mpp.put(pair.left, pair);
-                    q.offer(pair.left);
+                TreeNode qq = q.poll();
+                if(qq.left!= null){
+                    mpp.put(qq.left, qq);
+                    q.offer(qq.left);
                 }
-                if(pair.right != null){
-                    mpp.put(pair.right,pair);
-                    q.offer(pair.right);
+                if(qq.right!= null){
+                    mpp.put(qq.right, qq);
+                    q.offer(qq.right);
                 }
-                
+
             }
         }
-        int value =0;
+        int value = 0;
         q1.offer(target);
         visited.put(target,true);
         while(!q1.isEmpty()){
-            int ss = q1.size();
-            
             if(value == k){
                 break;
             }
-            for(int i =0; i<ss; i++){
-                TreeNode qq = q1.poll();
-                
-                if( qq.left!= null && !visited.containsKey(qq.left)){
-                    q1.offer(qq.left);
-                    visited.put(qq.left,true);
+            int si = q1.size();
+            
+            for(int i =0; i<si; i++){
+                TreeNode nn = q1.poll();
+                if( nn.left!= null && !visited.containsKey(nn.left)){
+                    
+                    q1.offer(nn.left);
+                    visited.put(nn.left,true);
                 }
-                if( qq.right!= null && !visited.containsKey(qq.right)  ){
-                    q1.offer(qq.right);
- visited.put(qq.right,true);
+                if(nn.right!= null && !visited.containsKey(nn.right)){
+                    
+                    q1.offer(nn.right);
+                    visited.put(nn.right,true);
                 }
-                if(mpp.containsKey(qq) && !visited.containsKey(mpp.get(qq))){
-                   q1.offer(mpp.get(qq));
-                   visited.put(mpp.get(qq),true);
+                if(mpp.containsKey(nn) && !visited.containsKey(mpp.get(nn))){
+                    TreeNode val = mpp.get(nn);
+                    q1.offer(val);
+                    visited.put(val, true);
                 }
                 
-                
-                // if(parent.containsKey(qq) && !visited.containsKey(parent.get(qq))) {
-                //     q1.add(parent.get(qq));
-                //     visited.put(parent.get(qq), true);
-                // }
             }
             value++;
-            
-            
         }
-            
-            List<Integer> list = new ArrayList<>();
-            
-            for(TreeNode tree  : q1){
-                list.add(tree.val);
-            }
-             
-        
-       return list;
+         List<Integer>   ans = new ArrayList<>();
+         for(TreeNode tr : q1){
+           
+            ans.add(tr.val);
+         }
+         return ans;
     }
 }
