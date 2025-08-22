506b995f0c81034fae49cae7293b61c5182df88a
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-08-22 21:53:57 +0530

    Time: 0 ms (100%), Space: 41.9 MB (83.69%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,31 +1,40 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start =0;
-        int n = nums.length;
-        int end = n -1;
-        int ans =-1;
-        while(start <= end){
-            int mid = start + (end - start)/2;
-
-            if(nums[mid] == target){
-                return mid;
-            }
-            if(nums[start] <= nums[mid]){
-                if(nums[start] <= target && target < nums[mid]){
-                end = mid -1;}
-                else{
-                start = mid +1;
-                }
-            }
-            else{
-                if(target >= nums[mid] && target <= nums[end]){
-                    start  = mid +1;
-                }
-                else{
-                    end = mid -1;
-                }
-            }
+    //   int n = nums.length;
+    //   int low = 0;
+    //   int high = n-1;
+    //   Arrays.sort(nums);
+    //   int ans =0;
+    //   while(low <= high){
+    //     int mid = low + (high - low)/2;
+    //     if(nums[mid] == target){
+    //         ans = mid;
+    //         break;
+    //     }
+    //     else if(target > nums[mid]){
+    //         low = mid +1;
+    //     }
+    //     else{
+    //         high = mid -1;
+    //     }
+    //   }
+    //   return ans;
+    int n = nums.length;
+    int ans =0;
+    boolean flag = false;
+    for(int i =0; i<n; i++){
+        
+        if(target == nums[i]){
+            ans = i;
+            flag = true;
         }
-        return ans;
+        
+    }
+    if(flag){
+    return ans;
+    }
+    else{
+        return -1;
+    }
     }
 }
