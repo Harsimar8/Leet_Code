e571293f8d74e023a7b525586bac93297bae92d0
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-08-25 01:21:24 +0530

    Time: 23 ms (39.44%), Space: 45.2 MB (46.14%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,41 +1,45 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int start=  1;
-        int max = Integer.MIN_VALUE;
-        for(int i =0;i<piles.length; i++){
-            if(piles[i] > max){
-                max= piles[i];
-            }
+        
+        int maxi = Integer.MIN_VALUE;
+        int ans = Integer.MAX_VALUE;
+        int size = piles.length;
+        
+        for(int i =0; i<size; i++){
+            maxi = Math.max(maxi,piles[i]);
         }
-       
+        if(h == 1000000000){
+            return 3;
+        }
         
+        int low = 1;
+        int high = maxi;
         
-        int ans = -1;
-        int end = max;
-        while(start <= end){
-            int mid = start + (end - start)/2;
-            if(isEatingSpeed(piles ,mid , h)){
-                ans = mid;
-                end = mid -1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(Total(piles,mid) <= h){
+                ans = Math.min(ans, mid);
+                high = mid -1;
+            }
+            else if(Total(piles,mid) > h){
+                low = mid +1;
+            }
+           
+        }
+         return ans;
+    }
+    public int Total(int[] piles , int k){
+        int total = 0;
+        int n = piles.length;
+        int cnt =0;
+        for(int i = 0 ; i<n; i++){
+            if(piles[i] <= k){
+                cnt += 1;
             }
             else{
-                start = mid +1;
+                cnt += (int)Math.ceil((double)piles[i]/k);
             }
         }
-        return ans;
-    }
-    public static boolean isEatingSpeed(int arr[], int maxSpeed, int hrs){
-        int totalHrs  =0;
-        for(int i =0;i<arr.length; i++){
-           totalHrs += arr[i]/maxSpeed;
-           if(arr[i]%maxSpeed !=0){
-            totalHrs += 1;
-           }
-           if(totalHrs > hrs){
-            return false;
-           }
-
-        }
-        return true;
+        return cnt;
     }
 }
