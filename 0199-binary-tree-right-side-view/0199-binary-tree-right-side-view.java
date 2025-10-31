440708bed27c01f147fd56f5b11888fa38fd8b86
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-10-31 13:31:49 +0530

    Time: 0 ms (100%), Space: 43.5 MB (8.66%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -15,34 +15,21 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-         Map<Integer, Integer> mpp = new TreeMap<>();
-        Queue<Pair<TreeNode,Integer>> q = new LinkedList<>();
         List<Integer> ans = new ArrayList<>();
-        if(root == null){
-            return ans;
-        }
-        q.offer(new Pair<>(root,0));
-        int line =0;
-       
-        while(!q.isEmpty()){
-            Pair<TreeNode, Integer> pair = q.poll();
-            TreeNode noo = pair.getKey();
-            line = pair.getValue();
-            
-            mpp.put(line, noo.val);
-            
-           
-            line++;
-            if(noo.left!= null){
-                q.offer(new Pair(noo.left, line));
-            }
-            if(noo.right != null){
-                q.offer(new Pair(noo.right, line));
-            }
-        }
-        for(int val : mpp.values()){
-            ans.add(val);
-        }
+        int level = 0;
+        rightV(root, level, ans);
         return ans;
     }
+    public void rightV(TreeNode root, int level, List<Integer> ans){
+        if(root == null){
+            return;
+        }
+        if(ans.size() == level){
+            ans.add(root.val);
+        }
+        level++;
+        rightV(root.right, level, ans);
+        rightV(root.left, level, ans);
+    }
+
 }
