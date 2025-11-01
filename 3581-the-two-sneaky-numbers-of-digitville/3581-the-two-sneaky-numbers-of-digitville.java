08049d5ab96e4e03e36d7198f21e6fb8641f25da
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-11-02 01:28:43 +0530

    Time: 3 ms (32.21%), Space: 46.8 MB (6.96%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
@@ -1,13 +1,19 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        int n = nums.length;
-        boolean[] no = new boolean[n];
-        int[] ans = new int[2];
-        int k = 0;
-        for(int i = 0;i<n;i++){
-            if(no[nums[i]]==true) ans[k++] = nums[i];
-            else no[nums[i]] = true;
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        for(int i =0; i<nums.length;i++){
+            mpp.put(nums[i],mpp.getOrDefault(nums[i],0) +1);
         }
-        return ans;
+        int i =0;
+        int[] arr= new int[2];
+        for(int a : mpp.keySet()){
+            if(mpp.get(a) == 2){
+                int val =a;
+                arr[i] = a;
+                if(i<2){
+                i++;}
+            }
+        }
+        return arr;
     }
 }
