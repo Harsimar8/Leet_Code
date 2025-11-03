4c12092e58ddcc4fb6240ed64711ea3e1053f2fc
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-11-03 16:01:44 +0530

    Time: 33 ms (24.14%), Space: 147.2 MB (5.52%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
@@ -1,47 +1,88 @@
-import java.util.*;
-
 class Solution {
-    void dfs(int r, int c, String dir, int[][] vis, Map<String, Integer> mpp) {
-        int n = vis.length;
-        int m = vis[0].length;
-        if (r < 0 || c < 0 || r >= n || c >= m) return;
-        if (mpp.containsKey(r + "," + c)) return;
-        vis[r][c] = 1;
-
-        if (dir.equals("r")) dfs(r, c + 1, "r", vis, mpp);
-        if (dir.equals("l")) dfs(r, c - 1, "l", vis, mpp);
-        if (dir.equals("u")) dfs(r - 1, c, "u", vis, mpp);
-        if (dir.equals("d")) dfs(r + 1, c, "d", vis, mpp);
-    }
-
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        int[][] vis = new int[m][n];
-        Queue<int[]> q = new LinkedList<>();
-        Map<String, Integer> mpp = new HashMap<>();
+        int[][] board = new int[m][n];
 
-        for (int[] g : guards) {
-            q.add(g);
-            mpp.put(g[0] + "," + g[1], 1);
-            vis[g[0]][g[1]] = 1;
+        for(int[] pos : guards){
+            int r = pos[0];
+            int c = pos[1];
+            board[r][c] = 'G';
         }
 
-        for (int[] w : walls) {
-            mpp.put(w[0] + "," + w[1], 1);
-            vis[w[0]][w[1]] = 1;
+        for(int[] pos : walls){
+            int r = pos[0];
+            int c = pos[1];
+            board[r][c] = 'W';
         }
 
-        for (int[] a : guards) {
-            int r = a[0], c = a[1];
-            dfs(r, c + 1, "r", vis, mpp);
-            dfs(r, c - 1, "l", vis, mpp);
-            dfs(r + 1, c, "d", vis, mpp);
-            dfs(r - 1, c, "u", vis, mpp);
+        for(int i =0; i<m;i++){
+            boolean found = false;
+            for(int j =0; j<n; j++){
+                if(board[i][j] == 'W'){
+                    found = false;
+                }
+                else if(board[i][j] == 'G'){
+                    found = true;
+                }
+                else if(found){
+                    board[i][j] = 'V';
+                }
+            }
         }
 
-        int cnt = 0;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (vis[i][j] == 0) cnt++;
+        for(int i =0; i<m;i++){
+            boolean found = false;
+            for(int j =n-1; j>=0; j--){
+                if(board[i][j] == 'W'){
+                    found = false;
+                }
+                else if(board[i][j] == 'G'){
+                    found = true;
+                }
+                else if(found){
+                    board[i][j] = 'V';
+                }
+            }
+        }
+
+        for(int j =0; j<n;j++){
+            boolean found = false;
+            for(int i=0; i<m; i++){
+                if(board[i][j] == 'W'){
+                    found = false;
+                }
+                else if(board[i][j] == 'G'){
+                    found = true;
+                }
+                else if(found){
+                    board[i][j] = 'V';
+                }
+            }
+        }
+
+        for(int j =n-1; j>=0;j--){
+            boolean found = false;
+            for(int i =m-1; i>=0; i--){
+                if(board[i][j] == 'W'){
+                    found = false;
+                }
+                else if(board[i][j] == 'G'){
+                   found = true;
+                }
+                else if(found){
+                    board[i][j] = 'V';
+                }
+            }
+        }
+
+        int cnt =0;
+        for(int i =0; i<m; i++){
+            for(int j =0; j<n; j++){
+                if(board[i][j] != 'V' && board[i][j] != 'G' && board[i][j] != 'W'){
+                    cnt++;
+                }
+            }
+        }
         return cnt;
+
     }
 }
