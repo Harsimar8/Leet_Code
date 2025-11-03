963bd89fbb5e3da0a42d37b2a824e387f53e54e8
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-11-04 03:17:29 +0530

    Time: 6 ms (82.18%), Space: 46.5 MB (5.44%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
@@ -1,28 +1,40 @@
 class Solution {
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<int[]>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
-        for (int[] flight : flights) {
-            adj.get(flight[0]).add(new int[] {flight[1], flight[2]});
+    class Pair{
+        int a;
+        int b;
+        int c;
+        Pair(int a, int b, int c){
+            this.a = a;
+            this.b = b;
+            this.c = c;
         }
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {src, 0});
-        int[] minCost = new int[n];
-        Arrays.fill(minCost, Integer.MAX_VALUE);
-        int stops = 0;
-        while (!q.isEmpty() && stops <= k) {
-            int size = q.size();
-            while (size-- > 0) {
-                int[] curr = q.poll();
-                for (int[] neighbour : adj.get(curr[0])) {
-                    int price = neighbour[1], neighbourNode = neighbour[0];
-                    if (price + curr[1] >= minCost[neighbourNode]) continue;
-                    minCost[neighbourNode] = price + curr[1];
-                    q.offer(new int[] {neighbourNode, minCost[neighbourNode]});
+    }
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        List<List<int[]>> ans = new ArrayList<>();
+        for(int i =0; i<n;i++){
+            ans.add(new ArrayList<>());
+        }
+        for(int[] f : flights){
+            ans.get(f[0]).add(new int[]{f[1],f[2]});
+        }
+        Queue<Pair> q = new ArrayDeque<>();
+        q.offer(new Pair(0,src,0));
+        int[] dist = new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            int stops = p.a;
+            int node = p.b;
+            int cost = p.c;
+            for(int[] neigh : ans.get(node)){
+                int next = neigh[0];
+                int nextCost = neigh[1];
+                if(cost + nextCost < dist[next] && stops <= k){
+                    dist[next] = cost + nextCost;
+                    q.offer(new Pair(stops + 1, next, cost + nextCost));
                 }
             }
-            stops++;
         }
-        return minCost[dst] == Integer.MAX_VALUE ? -1 : minCost[dst];
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
     }
 }
