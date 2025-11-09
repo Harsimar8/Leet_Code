fe6c2e723188ae1c124f73e2e91a881a2bae5a1f
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-11-09 18:18:18 +0530

    Time: 1 ms (90.35%), Space: 42.3 MB (11.81%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.java
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.java
@@ -1,5 +1,22 @@
 class Solution {
-    public int countOperations(int a, int b) {
-        return (a == 0 || b == 0) ? 0 : a / b + countOperations(b, a % b);
+    public int countOperations(int num1, int num2) {
+        int cnt = 0;
+        while(num1!= 0 && num2 != 0){
+            if(num1 == 0 || num2 == 0){
+                break;
+            }
+            int sum = 0;
+            if(num1 >= num2){
+                sum = num1 - num2;
+                num1 = sum;
+                cnt++;
+            }
+            else{
+                sum = num2 - num1;
+                num2 = sum;
+                cnt++;
+            }
+        }
+        return cnt;
     }
 }
