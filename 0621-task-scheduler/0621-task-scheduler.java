c7036b0cd9c29125bbc8d6f69fb1ee84271f8ec2
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-11-13 11:30:55 +0530

    Time: 27 ms (38.8%), Space: 48.3 MB (5.62%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,16 +1,37 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         int[] freq = new int[26];
         for(char c : tasks){
             freq[c - 'A']++;
         }
-        Arrays.sort(freq);
-        int holes = freq[25] -1;
-        int slots = holes * n;
-        for(int i=24; i>=0 ;i--){
-            slots -= Math.min(holes, freq[i]);
-        }
-        return slots<0 ? tasks.length : tasks.length+slots;
         
+        for(int a : freq){
+            if(a > 0){
+                pq.add(a);
+            }
+        }
+        int total =0;
+        while(!pq.isEmpty()){
+            int cycle = n +1;
+            List<Integer> temp = new ArrayList<>();
+
+            while(cycle != 0 && !pq.isEmpty()){
+                int cur = pq.poll();
+                cur--;
+                if(cur > 0){
+                    temp.add(cur);
+                }
+                total++;
+                cycle--;
+            }
+            for(int t : temp){
+                pq.add(t);
+            }
+            if(!pq.isEmpty()){
+                total += cycle;
+            }
+        }
+        return total;
     }
 }
