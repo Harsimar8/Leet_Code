4d88fcfdfc752d037b68814ffb492945aa6420f7
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-11-18 16:59:10 +0530

    Time: 2 ms (71.24%), Space: 43.9 MB (26.79%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,47 +1,47 @@
+class Node{
+    int row;
+    int col;
+    int time;
+    Node(int row, int col, int time){
+        this.row = row;
+        this.col = col;
+        this.time = time;
+    }
+}
 class Solution {
+    int[][] directions = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+    public boolean isValid(int i, int j, int rows,  int cols){
+        return (i>=0 && j>=0 && i<rows && j<cols);
+    }
     public int orangesRotting(int[][] grid) {
-        
-        Queue<int[]> q = new LinkedList<>();
-        int cnt_F = 0;
-        int cnt = 0;
-        int time = 0;
         int m = grid.length;
         int n = grid[0].length;
-        for(int i =0; i<m; i++){
+        int fresh =0;
+        Queue<Node> q = new ArrayDeque<>();
+        for(int i =0; i<m;i++){
             for(int j =0; j<n; j++){
                 if(grid[i][j] == 2){
-                    q.offer(new int[]{i,j});
+                    q.offer(new Node(i,j,0));
                 }
-                if(grid[i][j] != 0){
-                    cnt_F++;
+                else if(grid[i][j] == 1){
+                    fresh++;
                 }
             }
         }
-        if(cnt_F == 0){
-            return 0;
-        }
-        int[] dx = {0,0,-1,1};
-        int[] dy = {-1,1,0,0};
+        int minTime = 0;
         while(!q.isEmpty()){
-            int size = q.size();
-            cnt += size;
-            for(int i =0; i<size; i++){
-                int[] point = q.poll();
-                for(int j =0; j<4; j++){
-                    int x = point[0] + dx[j];
-                    int y = point[1] + dy[j];
-
-                if(x < 0 || y < 0|| x >= m || y >= n || grid[x][y] == 2 || grid[x][y] == 0){
-                    continue;
-                    }
-                    grid[x][y] = 2;
-                    q.offer( new int[]{x,y});
+            Node curr = q.poll();
+            for(int[] dir: directions){
+                int newRow = curr.row + dir[0];
+                int newCol  = curr.col + dir[1];
+                if(isValid(newRow,newCol,m,n) && grid[newRow][newCol] == 1){
+                    q.offer(new Node(newRow,newCol, curr.time + 1));
+                    grid[newRow][newCol] = 2;
+                    fresh--;
+                    minTime = curr.time + 1;
                 }
             }
-            if(q.size() != 0){
-                time++;
-            }
         }
-        return (cnt_F == cnt) ? time : -1;
+        return fresh == 0 ? minTime : -1;
     }
 }
