0b13cc9f00182b10200a27d5635f9907b5169d7e
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-11-19 19:25:04 +0530

    Time: 352 ms (42.66%), Space: 47.7 MB (9.05%) - LeetHub

--- 3479-count-the-number-of-substrings-with-dominant-ones/3479-count-the-number-of-substrings-with-dominant-ones.java
+++ 3479-count-the-number-of-substrings-with-dominant-ones/3479-count-the-number-of-substrings-with-dominant-ones.java
@@ -1,31 +1,51 @@
 class Solution {
     public int numberOfSubstrings(String s) {
         int n = s.length();
-        int[] dp = new int[n + 1];
-        java.util.Arrays.fill(dp, -1);
+        int[] pref = new int[n + 1];
+        for (int i = 0; i < n; i++)
+            pref[i + 1] = pref[i] + (s.charAt(i) == '1' ? 1 : 0);
 
-        for (int i = 0; i < n; i++) {
-            if (i == 0 || s.charAt(i - 1) == '0')
-                dp[i + 1] = i;
-            else
-                dp[i + 1] = dp[i];
+        List<Integer> Z = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            if (s.charAt(i) == '0') Z.add(i);
+        int m = Z.size();
+
+        long ans = 0;  
+
+        int i = 0;
+        while (i < n) {
+            if (s.charAt(i) == '0') { i++; continue; }
+            int j = i;
+            while (j < n && s.charAt(j) == '1') j++;
+            long len = j - i;
+            ans += len * (len + 1) / 2;
+            i = j;
         }
 
-        int res = 0;
+        int B = (int)Math.sqrt(n) + 2;
 
-        for (int i = 1; i <= n; i++) {
-            int cnt0 = (s.charAt(i - 1) == '0') ? 1 : 0;
-            int j = i;
+        for (int a = 0; a < m; a++) {
+            int Lmin = (a == 0 ? 0 : Z.get(a - 1) + 1), Lmax = Z.get(a);
+            if (Lmin > Lmax) continue;
 
-            while (j > 0 && cnt0 * cnt0 <= n) {
-                int cnt1 = (i - dp[j]) - cnt0;
-                if (cnt0 * cnt0 <= cnt1) {
-                    res += Math.min(j - dp[j], cnt1 - cnt0 * cnt0 + 1);
+            for (int z = 1; z <= B; z++) {
+                int b = a + z - 1;
+                if (b >= m) break;
+
+                int Rmin = Z.get(b), Rmax = (b + 1 < m ? Z.get(b + 1) - 1 : n - 1);
+                if (Rmin > Rmax) continue;
+
+                int need = z * z, r = Rmin;
+
+                for (int l = Lmin; l <= Lmax; l++) {
+                    if (pref[Rmax + 1] - pref[l] < need) continue;
+                    while (r <= Rmax && pref[r + 1] - pref[l] < need) r++;
+                    if (r > Rmax) break;
+                    ans += (Rmax - r + 1);
                 }
-                j = dp[j];
-                cnt0++;
             }
         }
-        return res;
+
+        return (int) ans;  
     }
 }
