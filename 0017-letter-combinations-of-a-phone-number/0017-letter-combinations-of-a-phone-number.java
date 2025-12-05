38a3f3ccd46d62deb18699e0698ad4c5e1a72686
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-12-05 15:04:43 +0530

    Time: 3 ms (34.99%), Space: 49.1 MB (6.71%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,10 +1,7 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        List<String> ans =new ArrayList<>();
-        HashMap<Character, String> mpp = new HashMap<>();
-        if(digits.length() == 0){
-            return ans;
-        }
+        List<String> ans = new ArrayList<>();
+        HashMap<Character,String> mpp = new HashMap<>();
         mpp.put('2',"abc");
         mpp.put('3',"def");
         mpp.put('4',"ghi");
@@ -13,8 +10,10 @@ class Solution {
         mpp.put('7',"pqrs");
         mpp.put('8',"tuv");
         mpp.put('9',"wxyz");
+
         ans.add("");
-        for(char digit : digits.toCharArray()){
+        char[] arr = digits.toCharArray();
+        for(char digit : arr){
             String letter = mpp.get(digit);
             List<String> temp = new ArrayList<>();
             for(String s : ans){
@@ -23,7 +22,7 @@ class Solution {
                 }
             }
             ans = temp;
-        }
+        } 
         return ans;
     }
 }
