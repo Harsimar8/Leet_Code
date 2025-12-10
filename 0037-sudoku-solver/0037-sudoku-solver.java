523d03d6976f9c5fa36feff344602cfe11053430
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-12-11 02:38:57 +0530

    Time: 182 ms (67.74%), Space: 42.9 MB (14.79%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,48 +1,56 @@
 class Solution {
-    public boolean isValid(int m, int n, char c, char[][] board){
-        for(int i =0; i < 9; i++){
-            if(board[m][i] == c){
-                return false;
-            }
-        }
-        for(int j =0; j<9; j++){
-            if(board[j][n] == c){
-                return false;
-            }
-        }
-        int rowS = 3 * (m/3);
-        int colS = 3 * (n/3);
-       for(int r = 0; r < 3; r++){
-        for(int s = 0; s < 3; s++){
-            if(board[rowS+r][colS+s]  == c){
-                return false;
-            }
-        }
-       }
-       return true;
+    public void solveSudoku(char[][] board) {
+        int r  =board.length;
+        int c = board[0].length;
+        helper(board, 0,0,9,9);
     }
-    public boolean solveSudoku(char[][] board) {
-        for(int i = 0; i<9; i++){
-            for(int j =0; j<9; j++){
-                if(board[i][j] == '.'){
-                    for(char c = '1'; c <= '9'; c++){
-                        if(isValid(i,j,c,board)){
-                        board[i][j] = c;
-                        if(solveSudoku(board)){
-                            return true; 
-                        }
-                        else{
-                        board[i][j] = '.';
-                        }
-                        }
-                        
-                    }
+    public boolean helper(char[][] board, int i, int j, int r, int c){
+        if(j == c){
+            return helper(board,i+1,0,r,c);
+        }
+        if(i == r){
+            return true;
+        }
+        if(board[i][j] != '.'){
+            return helper(board,i,j+1,r,c);
+        }
+        boolean val = false;
+        for(char p= '1'; p<='9'; p++){
+            
+            if(check(board,i,j,r,c,p)){
+                board[i][j] = p;
+                if(helper(board,i,j+1,r,c)){
+                    return true;
+                }
+                 board[i][j] = '.';   
+            }
+             
+        }    
+         return false;
+            
+    }
+    
+    public boolean check(char[][] board, int i, int j, int r, int c,char p){
+        for(int k =0; k<c; k++){
+            if(board[i][k] == p){
+                return false;
+            }
+        }
+        for(int l =0; l<r; l++){
+            if(board[l][j] == p){
+                return false;
+            }
+        }
+        int sR = (i/3) * 3;
+        int eR = (j/3) * 3;
+        
+        for(int aa = sR; aa<sR +3; aa++){
+            for(int bb = eR; bb<eR +3; bb++){
+                if(board[aa][bb] == p){
                     return false;
                 }
-                
             }
         }
         return true;
-
     }
 }
