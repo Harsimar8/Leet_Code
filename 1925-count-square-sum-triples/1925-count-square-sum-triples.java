734936c6cbd8ed415cd60d5302dbe6c443e45392
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-12-10 05:38:17 +0530

    Time: 13 ms (62.14%), Space: 42.1 MB (58.41%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.java
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -3,22 +3,14 @@ class Solution {
         int cnt =0;
         for(int i =1; i<=n; i++){
             for(int j =1;j <=n; j++){
-                for(int k =1; k<=n; k++){
-                    if(isValid(i,j,k)){
-                        cnt++;
-                    }
+                int sum = i * i + j* j;
+                int c = (int)Math.sqrt(sum);
+                if(c * c == sum && c <= n){
+                    cnt++;
                 }
             }
         }
         return cnt;
     }
-    public boolean isValid(int i, int j, int k){
-        int a = i *i;
-        int b =j *j;
-        int c = k *k;
-        if(a + b == c){
-            return true;
-        }
-        return false;
-    }
+    
 }
