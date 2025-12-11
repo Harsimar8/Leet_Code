1595d2a828c95231c3f4cce4944bb229a2c9ba73
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-12-11 07:17:27 +0530

    Time: 185 ms (67.06%), Space: 42.1 MB (61.34%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,10 +1,8 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        int r  =board.length;
-        int c = board[0].length;
-        helper(board, 0,0,9,9);
+        helper(board,0,0,9,9);
     }
-    public boolean helper(char[][] board, int i, int j, int r, int c){
+    public boolean helper(char[][] board ,int i, int j, int r, int c){
         if(j == c){
             return helper(board,i+1,0,r,c);
         }
@@ -14,23 +12,18 @@ class Solution {
         if(board[i][j] != '.'){
             return helper(board,i,j+1,r,c);
         }
-        boolean val = false;
-        for(char p= '1'; p<='9'; p++){
-            
+        for(char p = '1'; p<='9'; p++){
             if(check(board,i,j,r,c,p)){
                 board[i][j] = p;
                 if(helper(board,i,j+1,r,c)){
                     return true;
                 }
-                 board[i][j] = '.';   
+                board[i][j] = '.';
             }
-             
-        }    
-         return false;
-            
+        }
+        return false;
     }
-    
-    public boolean check(char[][] board, int i, int j, int r, int c,char p){
+    public boolean check(char[][] board ,int i, int j, int r, int c, char p){
         for(int k =0; k<c; k++){
             if(board[i][k] == p){
                 return false;
@@ -41,16 +34,17 @@ class Solution {
                 return false;
             }
         }
+
         int sR = (i/3) * 3;
         int eR = (j/3) * 3;
-        
-        for(int aa = sR; aa<sR +3; aa++){
-            for(int bb = eR; bb<eR +3; bb++){
+        for(int aa = sR; aa< sR +3; aa++){
+            for(int bb = eR; bb < eR +3;bb++){
                 if(board[aa][bb] == p){
                     return false;
                 }
             }
         }
+
         return true;
     }
 }
