021ffd99a3cfd39d6dbe0c85493d6d32ec1f59d9
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-12-22 13:11:27 +0530

    Time: 12 ms (99.23%), Space: 215.4 MB (67.65%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.java
+++ 3531-count-covered-buildings/3531-count-covered-buildings.java
@@ -1,28 +1,27 @@
 class Solution {
     public int countCoveredBuildings(int n, int[][] buildings) {
-        Map<Integer, int[]> yRangeGivenX = new HashMap<>();
-        Map<Integer, int[]> xRangeGivenY = new HashMap<>();
-        
-        for (int[] b : buildings) {
-            int x = b[0], y = b[1];
-            yRangeGivenX.putIfAbsent(x, new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE});
-            yRangeGivenX.get(x)[0] = Math.min(yRangeGivenX.get(x)[0], y);
-            yRangeGivenX.get(x)[1] = Math.max(yRangeGivenX.get(x)[1], y);
-
-            xRangeGivenY.putIfAbsent(y, new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE});
-            xRangeGivenY.get(y)[0] = Math.min(xRangeGivenY.get(y)[0], x);
-            xRangeGivenY.get(y)[1] = Math.max(xRangeGivenY.get(y)[1], x);
+        int[] minr = new int[n+1];
+        int[] minc = new int[n+1];
+        int[] maxr = new int[n+1];
+        int[] maxc = new int[n+1];
+        Arrays.fill(minr,Integer.MAX_VALUE);
+        Arrays.fill(minc, Integer.MAX_VALUE);
+        for(int[] b : buildings){
+            int x = b[0];
+            int y = b[1];
+            minr[y] = Math.min(minr[y],x);
+            minc[x] = Math.min(minc[x],y);
+            maxr[y] = Math.max(maxr[y],x);
+            maxc[x] = Math.max(maxc[x],y);
         }
-        
-        int count = 0;
-        for (int[] b : buildings) {
-            int x = b[0], y = b[1];
-            if (xRangeGivenY.get(y)[0] < x && x < xRangeGivenY.get(y)[1] &&
-                yRangeGivenX.get(x)[0] < y && y < yRangeGivenX.get(x)[1]) {
-                count++;
+        int cnt =0;
+        for(int[] a : buildings){
+            int x = a[0];
+            int y = a[1];
+            if(x > minr[y] && x < maxr[y] && y > minc[x] && y < maxc[x]){
+                cnt++;
             }
         }
-        
-        return count;
+        return cnt;
     }
 }
