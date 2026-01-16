5363957d438fe04941402fbdfc87bbafa2ed9128
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-01-16 11:56:38 +0530

    Time: 4 ms (26.18%), Space: 47.8 MB (23.61%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,26 +1,28 @@
 class Solution {
-    public int candy(int[] ratings) {
-        int n = ratings.length;
-        int[] arr = new int[n];
-        Arrays.fill(arr,1);
-        int maxi = Integer.MIN_VALUE;
-        for(int i = 1; i<n; i++){
-            if(ratings[i-1] < ratings[i]){
-                int val = arr[i-1];
-                arr[i] = val +1;
+    public int candy(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, 1);
+        for(int i =1; i<n; i++){
+            if(arr[i] > arr[i-1]){
+                ans[i] = ans[i-1] +1;
             }
         }
-        for(int i= n-2; i>=0; i--){
-            if(ratings[i] > ratings[i+1]){
-                int val = arr[i+1] +1;
-                maxi = Math.max(arr[i], val);
-                arr[i] = maxi;
+        
+        for(int i = n-2; i>=0; i--){
+            if(arr[i] > arr[i+1]){
+                int val = ans[i+1] +1;
+                if(val > ans[i]){
+                    ans[i] = val;
+                }
+                
             }
         }
-        int total = 0;
-        for(int i =0; i<arr.length; i++){
-            total += arr[i];
+        int total =0;
+        for(int k = 0; k<ans.length; k++){
+            total += ans[k];
         }
         return total;
+
     }
 }
