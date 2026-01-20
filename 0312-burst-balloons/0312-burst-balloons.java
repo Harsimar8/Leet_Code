fc34a5edd3691348e4683cae23e83f341c0a8744
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-01-20 22:35:01 +0530

    Time: 113 ms (34.24%), Space: 44.4 MB (56.6%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,36 +1,32 @@
 class Solution {
-    Integer[][] dp;
     public int maxCoins(int[] nums) {
         int n = nums.length;
-        int[] arr= new int[n + 2];
-         arr[0] = 1;
-         arr[n+1] = 1;
-         dp = new Integer[n+2][n+2];
-         for(int i =0; i<n;i++){
+        int[] arr = new int[n+2];
+        arr[0] = 1;
+        arr[n+1] = 1;
+        for(int i =0; i<n; i++){
             arr[i+1] = nums[i];
-         }
-        return helper(arr, 1, n);
+        }
+        int[][] dp = new int[n+2][n+2];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return helper(arr, 1, n, dp);
     }
-    public int helper(int[] arr, int i, int j){
-        if(i>j){
+    public int helper(int[] arr, int i, int j, int[][] dp){
+        if(i > j){
             return 0;
         }
-        int ans = 0;
-        if(dp[i][j] != null){
+        if(dp[i][j] != -1){
             return dp[i][j];
         }
+        int maxi = Integer.MIN_VALUE;
         for(int k =i; k<=j; k++){
-            int c1 = helper(arr,i,k-1);
-            int c2 = helper(arr,k+1, j);
-            int c3 = arr[i-1] * arr[k] * arr[j+1];
-            int tc = c1 + c2 + c3;
-            ans = Math.max(ans,tc);
-
+            int c1 = helper(arr, i,k-1,dp);
+            int c2 = helper(arr, k+1,j,dp);
+            int c3 = arr[i-1] * arr[j+1] * arr[k];
+            maxi = Math.max(maxi, c1 + c2 + c3);
         }
-        return dp[i][j] = ans;
+        return dp[i][j] = maxi;
     }
 }
-
-
-
-
