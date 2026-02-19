c7b2056b09c7f2d450178358c3837a6cf777f948
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-02-20 05:24:45 +0530

    Time: 3 ms (36.85%), Space: 45.8 MB (51.47%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,35 +1,33 @@
 class Solution {
-    public List<List<Integer>> ans = new ArrayList<>();
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<Integer> cur = new ArrayList<>();
-        int i=0;
-        helper(i,cur,candidates,target);
-        return ans;
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        Set<List<Integer>> st = new HashSet<>();
+        helper(st,res, ans, nums, target, 0);
+        return res;
     }
-    public void helper(int i , List<Integer> cur, int[] arr, int sum){
-        if(sum ==0){
-            ans.add(new ArrayList<>(cur));
+    public void helper(Set<List<Integer>> st, List<List<Integer>> res, List<Integer> ans,  int[] nums, int sum, int i){
+        if(sum == 0){
+           if(!st.contains(ans)){
+            res.add(new ArrayList<>(ans));
+            st.add(ans);
+           }
+            
             return;
         }
-        int n = arr.length -1;
-        if(i == n){
-            if(sum == arr[n]){
-                cur.add(arr[i]);
-                ans.add(new ArrayList<>(cur));
-                cur.remove(cur.size()-1); 
+        for(int j =i; j<nums.length; j++){
+            if(sum - nums[j] >= 0){
+                ans.add(nums[j]);
+                
+                helper(st,res,ans,nums,sum-nums[j],j);
+                ans.remove(ans.size() -1);
             }
-            else if(sum % arr[i] == 0){
-                cur.add(arr[i]);
-                helper(i,cur,arr,sum- arr[i]);
-                cur.remove(cur.size() -1);
-            }
-            return;
-        }
-        helper(i+1,cur,arr,sum);
-        if(arr[i] <= sum){
-            cur.add(arr[i]);
-            helper(i, cur,arr, sum - arr[i]);
-            cur.remove(cur.size() -1);
+            // if(sum != 0){
+            // helper(st,res,ans,nums,sum,j+1);
+            // }
+            
+            
+
         }
     }
 }
