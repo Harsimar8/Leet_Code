4ef71cde0781c15e1c983515f2051b35f243d10e
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-02-19 09:47:53 +0530

    Time: 595 ms (14.33%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,3 +1,7 @@
 # Write your MySQL query statement below
 
-select score, dense_rank() over (order by score desc) as `rank` from Scores order by score desc;
+-- select score, dense_rank() over (order by score desc) as `rank` from Scores order by score desc;
+
+
+select s1.score, count(distinct s2.score) + 1 as `rank` from scores as s1 Left join scores as s2 on s2.score > s1.score 
+group by s1.id, s1.score order by s1.score desc;
