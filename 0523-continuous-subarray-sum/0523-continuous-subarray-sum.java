e8400e5c96b12ca4045f53d48a94b4bd96ee168c
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-03-06 20:00:56 +0530

    Time: 23 ms (94.6%), Space: 140 MB (16.29%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,30 +1,28 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
         HashMap<Integer,Integer> mpp = new HashMap<>();
-        int n = nums.length;
         int right =0;
-        int pre =0;
-        int cnt =0;
         int left =0;
+        int preSum  =0;
+        int n = nums.length;
         mpp.put(0,-1);
-        while(right <n){
-            pre += nums[right];
-            int val = (pre % k);
-
-            if(k != 0){
-                pre%=k;
+        while(right < n){
+            preSum += nums[right];
+            if(k!=0){
+                preSum%=k;
             }
-            if(mpp.containsKey(pre)){
-                int ss = mpp.get(pre);
-                if(right - ss >= 2){
-                return true;
+            if(mpp.containsKey(preSum)){
+                int ss = mpp.get(preSum);
+                if(right - ss >=2){
+                    return true;
                 }
-            }else{
-                mpp.put(pre,right);
             }
-
-            
+            else{
+                mpp.put(preSum, right);
+            }
             right++;
+
+
         }
         return false;
     }
