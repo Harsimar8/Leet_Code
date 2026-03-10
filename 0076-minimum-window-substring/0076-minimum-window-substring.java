944c63468e36429353602f1c85f92dabdecc72ef
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-03-11 03:31:11 +0530

    Time: 2 ms (99.82%), Space: 46.3 MB (66.16%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -4,32 +4,33 @@ class Solution {
         for(char c : t.toCharArray()){
             freq[c]++;
         }
-        int left =0;
-        int miniLen = Integer.MAX_VALUE;
-        int right =0;
-        int n = s.length();
         int req = t.length();
+        int right = 0;
+        int left =0;
+        int mini = Integer.MAX_VALUE;
+        int n = s.length();
+        char[] ans = s.toCharArray();
         int start =0;
         while(right < n){
-            char cc = s.charAt(right);
-            if(freq[cc] > 0){
+            char val = ans[right];
+            freq[val]--;
+            if(freq[val] >= 0){
                 req--;
             }
             right++;
-            freq[cc]--;
             while(req == 0){
-                if(right - left < miniLen){
-                    miniLen = right - left;
-                    start =left;
+                if((right - left) < mini){
+                    mini = right - left;
+                    start = left;
                 }
-                char val = s.charAt(left);
-                freq[val]++;
-                if(freq[val] > 0){
+                char ss = ans[left];
+                freq[ss]++;
+                if(freq[ss] > 0){
                     req++;
                 }
                 left++;
             }
         }
-        return miniLen == Integer.MAX_VALUE ? "" : s.substring(start, start + miniLen);
+        return mini == Integer.MAX_VALUE ? "" : s.substring(start, start + mini);
     }
 }
