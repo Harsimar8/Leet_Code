cf3229562fc30cef5f530616fa863ca82fb98a73
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-03-13 02:25:13 +0530

    Time: 62 ms (62.31%), Space: 57.9 MB (50.18%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,36 +1,35 @@
 class Solution {
+    public int count =0;
     public int sumSubarrayMins(int[] arr) {
-        int n  = arr.length;
-        int[] prefix = new int[n];
-        Stack<Integer> st = new Stack<>();
-        Stack<Integer> st1 = new Stack<>();
+     long sum =0;
+     Stack<Integer> st1 = new Stack<>();
+     Stack<Integer> st2 = new Stack<>();
 
+     int n = arr.length;
+     int[] pre = new int[n];
+     int[] suf = new int[n];
+     for(int i =0; i<n; i++){
+        while(!st1.isEmpty() && arr[st1.peek()] >= arr[i]){
+            st1.pop();
+        }
+        pre[i] = (st1.isEmpty()) ? -1 : st1.peek();
+        st1.push(i);
+     }
+     for(int i =n-1; i>=0; i--){
+        while(!st2.isEmpty() && arr[st2.peek()] > arr[i]){
+            st2.pop();
+        }
+        suf[i] = (st2.isEmpty()) ? n : st2.peek();
+        st2.push(i);
+     }
+     int lod = (int)1e9 + 7;
+     for(int k =0; k<n; k++){
+        long pp = k - pre[k];
+        long oo = suf[k] - k;
 
-        for(int i =0; i<n; i++){
-            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
-                st.pop();
-            }
-            prefix[i] = (st.isEmpty()) ? -1 : st.peek();
-            st.push(i);
-        }
-         int[] suffix = new int[n];
-        for(int i =n-1; i>=0; i--){
-             while(!st1.isEmpty() && arr[st1.peek()] > arr[i]){
-                st1.pop();
-            }
-            suffix[i] = (st1.isEmpty()) ? n : st1.peek();
-            st1.push(i);
-        }
-        long sum =0;
-        int MODE = (int)1e9 + 7;
-        for(int i =0; i<n;i++){
-            long pp = i- prefix[i];
-            long rr = suffix[i] - i;
-            sum = (sum + (long)arr[i] * pp % MODE * rr % MODE)%MODE;
-            if(sum < 0){
-                sum += MODE;
-            }
-        }
-        return (int)sum;
+        sum  = (sum + (long)arr[k] * pp % lod * oo % lod) % lod;
+     }
+     return (int)sum;
     }
+   
 }
