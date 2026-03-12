21c09fcab74385ce95f1be214f2a61ef274ec6d1
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-03-13 01:57:56 +0530

    Time: 66 ms (39.89%), Space: 51.5 MB (95.26%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int n  = arr.length;
+        int[] prefix = new int[n];
+        Stack<Integer> st = new Stack<>();
+        Stack<Integer> st1 = new Stack<>();
+
+
+        for(int i =0; i<n; i++){
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
+                st.pop();
+            }
+            prefix[i] = (st.isEmpty()) ? -1 : st.peek();
+            st.push(i);
+        }
+         int[] suffix = new int[n];
+        for(int i =n-1; i>=0; i--){
+             while(!st1.isEmpty() && arr[st1.peek()] > arr[i]){
+                st1.pop();
+            }
+            suffix[i] = (st1.isEmpty()) ? n : st1.peek();
+            st1.push(i);
+        }
+        long sum =0;
+        int MODE = (int)1e9 + 7;
+        for(int i =0; i<n;i++){
+            long pp = i- prefix[i];
+            long rr = suffix[i] - i;
+            sum = (sum + (long)arr[i] * pp % MODE * rr % MODE)%MODE;
+            if(sum < 0){
+                sum += MODE;
+            }
+        }
+        return (int)sum;
+    }
+}
