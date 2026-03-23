d39f520012316bcd2f5db334b2c05bb38d6b3448
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-03-24 03:43:01 +0530

    Time: 103 ms (31.86%), Space: 86.5 MB (7.05%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,46 +1,37 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
         
         int idx =0;
         int idd =0;
-        int res =0;
-        Stack<Integer> st =new Stack<>();
-        for(int i =0; i<=n; i++){
-            int cur = (i == n) ? 0 : heights[i];
-            while(!st.isEmpty() && cur <= heights[st.peek()]){
-                int he = heights[st.pop()];
-                int right =i;
-                int left = (st.isEmpty()) ? -1 : st.peek();
-                res = Math.max( res, he * (right - left -1));
+       
+        Stack<Integer> st1 = new Stack<>();
+        for(int i =0; i<n; i++){
+            while(!st1.isEmpty() && heights[st1.peek()] >= heights[i]){
+                st1.pop();
             }
-            st.push(i);
+            int val = (st1.isEmpty()) ? -1 : st1.peek();
+            left[idx++] = val;
+            st1.push(i);
         }
-        // Stack<Integer> st1 = new Stack<>();
-        // for(int i =0; i<n; i++){
-        //     while(!st1.isEmpty() && heights[st1.peek()] >= heights[i]){
-        //         st1.pop();
-        //     }
-        //     int val = (st1.isEmpty()) ? -1 : st1.peek();
-        //     left[idx++] = val;
-        //     st1.push(i);
-        // }
-        // Stack<Integer> st2 = new Stack<>();
-        // for(int i =n-1; i>=0; i--){
-        //     while(!st2.isEmpty() && heights[st2.peek()] <= heights[i]){
-        //         st2.pop();
-        //     }
-        //     int val = (st2.isEmpty()) ? n : st2.peek();
-        //     right[idd++] = val;
-        //     st2.push(i);
-        // }
-        // int res = 0;
-        // for(int i =0; i<n; i++){
-        //     int val = left[i];
-        //     int cur = right[i];
-        //     int sum = heights[i] * (cur - val - 1);
-        //     res = Math.max(res, sum);
-        // }
+        Stack<Integer> st2 = new Stack<>();
+        for(int i =n-1; i>=0; i--){
+            while(!st2.isEmpty() && heights[st2.peek()] > heights[i]){
+                st2.pop();
+            }
+            int val = (st2.isEmpty()) ? n : st2.peek();
+            right[i] = val;
+            st2.push(i);
+        }
+        int res = 0;
+        for(int i =0; i<n; i++){
+            int val = left[i];
+            int cur = right[i];
+            int sum = heights[i] * (cur - val - 1);
+            res = Math.max(res, sum);
+        }
         return res;
 
     }
