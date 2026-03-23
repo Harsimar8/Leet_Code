648033b8c506b4b211d491a8a85441830bffb2cc
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-03-24 03:39:08 +0530

    Time: 59 ms (78%), Space: 76.4 MB (87.21%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,22 +1,47 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int maxi =0;
         int n = heights.length;
-        if(n == 1){
-            return heights[0];
-        }
-        Stack<Integer> st = new Stack<>();
+        
+        int idx =0;
+        int idd =0;
+        int res =0;
+        Stack<Integer> st =new Stack<>();
         for(int i =0; i<=n; i++){
-            int curr = (i == n) ? 0: heights[i];
-            while(!st.isEmpty() && curr <= heights[st.peek()]){
+            int cur = (i == n) ? 0 : heights[i];
+            while(!st.isEmpty() && cur <= heights[st.peek()]){
                 int he = heights[st.pop()];
-                int right = i;
+                int right =i;
                 int left = (st.isEmpty()) ? -1 : st.peek();
-                int width = right - left - 1;
-                maxi = Math.max(maxi, he * width); 
+                res = Math.max( res, he * (right - left -1));
             }
             st.push(i);
         }
-        return maxi;
+        // Stack<Integer> st1 = new Stack<>();
+        // for(int i =0; i<n; i++){
+        //     while(!st1.isEmpty() && heights[st1.peek()] >= heights[i]){
+        //         st1.pop();
+        //     }
+        //     int val = (st1.isEmpty()) ? -1 : st1.peek();
+        //     left[idx++] = val;
+        //     st1.push(i);
+        // }
+        // Stack<Integer> st2 = new Stack<>();
+        // for(int i =n-1; i>=0; i--){
+        //     while(!st2.isEmpty() && heights[st2.peek()] <= heights[i]){
+        //         st2.pop();
+        //     }
+        //     int val = (st2.isEmpty()) ? n : st2.peek();
+        //     right[idd++] = val;
+        //     st2.push(i);
+        // }
+        // int res = 0;
+        // for(int i =0; i<n; i++){
+        //     int val = left[i];
+        //     int cur = right[i];
+        //     int sum = heights[i] * (cur - val - 1);
+        //     res = Math.max(res, sum);
+        // }
+        return res;
+
     }
 }
