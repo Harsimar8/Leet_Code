7f53ea0db95ccfbf02cc116fa5bc912d8420add4
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-03-24 03:21:38 +0530

    Time: 1 ms (59.31%), Space: 48.9 MB (7.44%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,33 +1,23 @@
 class Solution {
     public int trap(int[] height) {
-        int total =0;
-        int maxi  =0;
-        int[] leftMax = prefix(height);
-        int[] rightMax = suffix(height);
         int n = height.length;
+        int[] left = new int[n];
+        left[0] = height[0];
+        Stack<Integer> st = new Stack<>();
+        int idx =0;
+        int[] right = new int[n];
+        right[n-1] = height[n-1];
+        int res =0;
+        for(int k =1; k<n; k++){
+            left[k] = Math.max(left[k-1], height[k]);
+        }
+        for(int m = n-2; m>=0; m--){
+            right[m] = Math.max(right[m+1], height[m]);
+        }
         for(int i =0; i<n; i++){
-            int left = leftMax[i];
-            int right = rightMax[i];
-            total += Math.min(left, right) - height[i];
+             
+            res += Math.min(left[i],right[i]) - height[i];
         }
-        return total;
-    }
-    public int[] prefix(int[] nums){
-        int n = nums.length;
-        int[] prefix= new int[n];
-        prefix[0] = nums[0];
-        for(int i = 1; i<n; i++){
-            prefix[i] = Math.max(prefix[i-1], nums[i]);
-        }
-        return prefix;
-    }
-    public int[] suffix(int[] nums){
-        int n = nums.length;
-        int[] suffix = new int[n];
-        suffix[n-1] = nums[n-1];
-        for(int i = n-2; i>=0; i--){
-            suffix[i] = Math.max(suffix[i+1], nums[i]);
-        }
-        return suffix;
+        return res;
     }
 }
