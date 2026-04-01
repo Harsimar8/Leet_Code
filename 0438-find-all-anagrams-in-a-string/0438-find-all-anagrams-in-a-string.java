1f0813ef2c8d764e99c3ac001fd2e5238dd783b7
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-02 05:19:20 +0530

    Time: 12 ms (56.58%), Space: 47.1 MB (36.01%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,34 +1,36 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> ans = new ArrayList<>();
-        int n = s.length();
-        int m = p.length();
-        int right= 0;
-        int cnt = m;
         int[] freq = new int[26];
-        for(int i =0; i<m; i++){
-            freq[p.charAt(i) - 'a']++;
-        }
+       
+        int cnt =p.length();
+        int right =0;
+        int n = s.length();
         int left =0;
-        
+        for(char pp : p.toCharArray()){
+            freq[pp -'a']++;
+            
+        }
+        List<Integer> ans = new ArrayList<>();
         while(right < n){
-            if(freq[s.charAt(right) - 'a'] > 0){
+            if(freq[s.charAt(right)-'a'] > 0){
                 cnt--;
-            }
+            } 
+            
             freq[s.charAt(right) - 'a']--;
             if(cnt == 0){
                 ans.add(left);
             }
-            right++;
+            
+             right++;
             if(right - left == p.length()){
-                if(freq[s.charAt(left) -'a'] >= 0){
+                if(freq[s.charAt(left) - 'a'] >= 0){
                     cnt++;
                 }
+                
                 freq[s.charAt(left) - 'a']++;
                 left++;
             }
-
-            
+           
         }
         return ans;
     }
