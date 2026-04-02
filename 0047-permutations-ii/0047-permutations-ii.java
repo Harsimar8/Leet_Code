e49894f17a44acfeed83f5aacc5c9562b7c1e6d7
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-02 20:09:07 +0530

    Time: 19 ms (15.23%), Space: 46.5 MB (91.78%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,29 +1,29 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        Set<List<Integer>> st = new HashSet<>();
-        List<List<Integer>> ans = new ArrayList<>();
-        boolean[] visi = new boolean[nums.length];
-        List<Integer> cur = new ArrayList<>();
-        back(st,ans,nums,visi,cur);
-        for(List<Integer> a : st){
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        for(int a : nums){
             ans.add(a);
         }
-        return ans;
+        int n = nums.length;
+        Set<List<Integer>> st = new HashSet<>();
+        helper(st, res, ans, nums, 0,n);
+        return res;
+
     }
-    public void back(Set<List<Integer>> st, List<List<Integer>> ans, int[] nums ,boolean[] visi,List<Integer> cur){
-        if(cur.size() == nums.length){
-            st.add(new ArrayList<>(cur));
+    public void helper(Set<List<Integer>> st, List<List<Integer>> res, List<Integer> ans, int[] nums, int i, int n){
+        if(i == n){
+            if(!st.contains(ans)){
+            res.add(new ArrayList<>(ans));
+            st.add(ans);
+            }
             return;
         }
-        for(int i =0; i<nums.length; i++){
-            if(visi[i]){
-                continue;
-            }
-            visi[i] = true;
-            cur.add(nums[i]);
-            back(st,ans,nums,visi,cur);
-            visi[i] = false;
-            cur.remove(cur.size() -1);
+        for(int k =i; k<n; k++){
+            Collections.swap(ans, i,k);
+            helper(st,res,ans, nums, i+1,n);
+            Collections.swap(ans, k,i);
         }
     }
+    
 }
