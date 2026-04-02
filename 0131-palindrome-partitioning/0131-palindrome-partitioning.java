2fdcde141f163432b0df0d122cb8d71f882d976b
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-03 03:09:42 +0530

    Time: 8 ms (74.64%), Space: 65.2 MB (81.51%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,29 +1,25 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList<>();
-        List<String> ans = new ArrayList<>();
-        helper(s, res, ans, 0);
-        return res;
+       List<List<String>> res = new ArrayList<>();
+       List<String> ans = new ArrayList<>();
+       helper(s,res, ans, 0,s.length());  
+       return res;
     }
-    public void helper(String s, List<List<String>> res, List<String> ans,  int i){
-        if(i == s.length()){
+    public void helper(String s, List<List<String>> res, List<String> ans, int i, int n){
+        if(i == n){
             res.add(new ArrayList<>(ans));
-            
             return;
         }
-        for(int j =i; j<s.length(); j++){
-            if(isPalin(s,i,j)){
-            ans.add(s.substring(i,j+1));
-            helper(s,res,ans,j+1);
-            ans.remove(ans.size() -1);
+        for(int k =i; k<n; k++){
+            if(isPalind(s, i,k)){
+                ans.add(s.substring(i,k+1));
+                helper(s, res,ans,k+1, n);
+                ans.remove(ans.size() -1);
             }
         }
     }
-    public boolean isPalin(String s, int left, int right){
-        if(s.length() == 1){
-            return true;
-        }
-        while(left < right){
+    public boolean isPalind(String s, int left, int right){
+        while(left <= right){
             if(s.charAt(left) != s.charAt(right)){
                 return false;
             }
