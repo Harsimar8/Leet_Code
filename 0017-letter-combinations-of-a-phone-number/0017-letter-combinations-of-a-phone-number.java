19863543191d715b25f02a042452007be9a27f4d
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-06 22:36:49 +0530

    Time: 3 ms (34.52%), Space: 49.2 MB (9.37%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,30 +1,29 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        HashMap<Character,String> mpp = new HashMap<>();
-        mpp.put('2',"abc");
-        mpp.put('3',"def");
-        mpp.put('4',"ghi");
-        mpp.put('5',"jkl");
-        mpp.put('6',"mno");
-        mpp.put('7',"pqrs");
-        mpp.put('8',"tuv");
-        mpp.put('9',"wxyz");
         List<String> ans = new ArrayList<>();
+        HashMap<Character, String> mpp = new HashMap<>();
+        mpp.put('2',"abc");
+        mpp.put('3', "def");
+        mpp.put('4', "ghi");
+        mpp.put('5', "jkl");
+        mpp.put('6',"mno");
+        mpp.put('7', "pqrs");
+        mpp.put('8', "tuv");
+        mpp.put('9',"wxyz");
+
         ans.add("");
-        char[] arr = digits.toCharArray();
-        for(char c : arr){
+        for(char c : digits.toCharArray()){
             String ff = mpp.get(c);
-            List<String> temp = new ArrayList<>();
-            for(char bb : ff.toCharArray()){
-                
-                for(String ss : ans){
-                    temp.add(ss + bb);
+             List<String> temp = new ArrayList<>();
+            for(char p : ff.toCharArray()){
+
+                for(String o : ans){
+                    temp.add(o + p);
                 }
                 
             }
-            ans  = temp;
-            
-        }  
+            ans = temp;
+        }
         return ans;
     }
 }
