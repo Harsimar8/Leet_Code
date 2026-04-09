f6132351e95f0a8ff418a8670d68a5d4bb899897
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-09 08:21:36 +0530

    Time: 1 ms (99.9%), Space: 45.5 MB (55.01%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -2,20 +2,21 @@ class Solution {
     public int compress(char[] chars) {
         int n = chars.length;
         int index =0;
-        HashMap<Character,Integer> mpp = new HashMap<>();
-        int i =0;
        
-        while(i<n){
-            char cur = chars[i];
-            int count =0;
-            while(i<n && chars[i] == cur){
+        HashMap<Character, Integer> mpp = new HashMap<>();
+        int i =0;
+
+        while(i < n){
+            char c = chars[i];
+            int cnt =0;
+            while(i < n && chars[i] == c){
                 i++;
-                count++;
+                cnt++;
             }
-            chars[index++] = cur;
-            if(count > 1){
-                for(char c : String.valueOf(count).toCharArray()){
-                    chars[index++] = c;
+            chars[index++] = c;
+            if(cnt > 1){
+                for(char p : String.valueOf(cnt).toCharArray()){
+                    chars[index++] = p;
                 }
             }
         }
