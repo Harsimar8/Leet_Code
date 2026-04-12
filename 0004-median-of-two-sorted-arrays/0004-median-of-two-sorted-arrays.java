b591cc8296fdec678a9ab58fd0094e12a2653397
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-12 16:54:46 +0530

    Time: 8 ms (14.72%), Space: 48.9 MB (39.1%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,41 +1,29 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        
-        if(nums1.length > nums2.length){
-            return findMedianSortedArrays(nums2,nums1);
+        ArrayList<Integer> ans = new ArrayList<>();
+        int one = nums1.length;
+        int two = nums2.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int ok : nums1){
+            pq.add(ok);
+        }       
+        for(int ko : nums2){
+            pq.add(ko);
         }
-        int n1= nums1.length;
-        int n2 = nums2.length;
-        int N = n1 + n2;
-        int s = 0;
-        int e = n1;
-        int k = N/2 ;
-        while(s <= e){
-            int m = s + (e - s)/2;
-            int cut1 = m;
-            int cut2 = k - m;  
-            int l1 = (cut1==0)?Integer.MIN_VALUE:nums1[cut1-1];
-            int l2 = (cut2==0)?Integer.MIN_VALUE:nums2[cut2-1];
-            int r1 = (cut1==n1)?Integer.MAX_VALUE:nums1[cut1];
-            int r2 = (cut2==n2)?Integer.MAX_VALUE:nums2[cut2];
-
-            if(l1<=r2 && l2<=r1){
-                 if(N%2!=0){
-                return Math.min(r1,r2);
-            }
-            else{
-                return (Math.max(l1,l2) + Math.min(r1,r2))/2.0;
-            }   
-            }
-            else if(l1>r2){
-                e = m -1;
-            }
-            else{
-                s = m+1;
-            }
-           
-
+        while(!pq.isEmpty()){
+            int val = pq.poll();
+            ans.add(val);
         }
-        return 0.0;
+        int sum = one + two;
+        if(sum % 2 != 0){
+            int mid = (one + two)/2;
+            return (double)ans.get(mid);      
+        }
+        else{
+            int mid = (one + two)/2;
+            int mid1 = mid - 1;
+            double last = (double)(ans.get(mid) + ans.get(mid1))/2;
+            return last;
+        }
     }
 }
