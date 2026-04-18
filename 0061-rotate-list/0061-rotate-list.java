737b5733dde8d7ff32bdae8044d7e7ba452ca2db
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-18 17:12:40 +0530

    Time: 0 ms (100%), Space: 44.4 MB (49.17%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,44 +10,28 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null || head.next == null){
-            return head;
-        }
-        ArrayList<Integer> arr = new ArrayList<>();
-        ListNode temp = head;
-        while(temp != null){
-            arr.add(temp.val);
-            temp = temp.next;
-        }
-        int ind = 0;
-        int[] res = new int[arr.size()];
-        for(int a : arr){
-            res[ind++] = a;
-        }
-        
-        int n = arr.size();
-        k = k%n;
+       if(k == 0|| head == null || head.next == null){
+        return head;
+       }
+       ListNode tail = head;
+       ListNode cur = head;
+       int n = 1;
+       while(tail.next != null){
+        n++;
+        tail = tail.next;
+       }
+       k=k%n;
+       if(k == 0){
+        return head;
+       }
+       for(int i =1; i<n-k;i++){
+        cur = cur.next;
+       }
+       ListNode newH = cur.next;
+       cur.next = null;
+       tail.next = head;
+       return newH;
 
-        reverse(res,0,n-1);
-        reverse(res,0,k-1);
-        reverse(res,k,n-1);
-        ListNode dummy  = new ListNode(-1);
-        ListNode ans = dummy;
-        for(int num : res){
-            ans.next = new ListNode(num);
-            ans = ans.next;
-        }
-        return dummy.next;
     }
-    public void reverse(int[] nums, int s, int e){
-        int n = nums.length;
-        while(s <= e){
-            int temp = nums[s];
-            nums[s] = nums[e];
-            nums[e] = temp;
-            s++;
-            e--;
-        }
-        
-    }
+    
 }
