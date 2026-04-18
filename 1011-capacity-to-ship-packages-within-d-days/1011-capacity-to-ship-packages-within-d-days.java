0977f3671a402d0c5c70d8ec5485c4854f5ca1b5
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-18 14:12:06 +0530

    Time: 10 ms (86.36%), Space: 49.8 MB (93.43%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -2,19 +2,22 @@ class Solution {
     public int shipWithinDays(int[] weights, int days) {
         int total =0;
         int n = weights.length;
-        int maxi  = 0;
-        for(int i = 0; i<n; i++){
+        int maxi =0;
+        for(int i =0; i<n; i++){
             total += weights[i];
             maxi = Math.max(maxi, weights[i]);
-        }   
+        }
+        if(days == 1){
+            return total;
+        }
         int low = maxi;
         int high = total;
-        int ans =0;
+        int ans = 0;
         while(low <= high){
-            int mid = low + (high - low)/2;
-            if(isPossible(weights, days, mid)){
+            int mid = (low + high)/2;
+            if(helper(weights, days, mid)){
                 ans = mid;
-                high = mid -1;
+                high = mid - 1;
             }
             else{
                 low = mid +1;
@@ -22,32 +25,20 @@ class Solution {
         }
         return ans;
     }
-
-    public boolean isPossible(int[] arr, int d, int sol){
-        int j =0;
-        int days =0;
-        int n = arr.length;
-        while(j < arr.length){
-            if(j >= arr.length){
-                break;
+    public boolean helper(int[] weights, int days, int k){
+        int d = 1;
+        int n = weights.length;
+        int sum = 0;
+        for(int i =0; i<n; i++){
+            if(sum + weights[i] > k){
+                d++;
+                sum = weights[i];
             }
-            int sum  =0;
-            
-            while(sum <= sol && j < n){
-                if(arr[j] + sum > sol){
-                    break;
-                }
-                sum += arr[j];
-                j++;
+            else{
+                sum += weights[i];
             }
-            days++;
-            
         }
-        if(days <= d){
-            return true;
-        }
-        else{
-            return false;
-        }
+        
+        return (d <= days);
     }
 }
