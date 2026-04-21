6c58b0eefad8f3f559c06ffcbf50203108930131
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-21 19:26:14 +0530

    Time: 53 ms (16.83%), Space: 48.9 MB (90.61%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,30 +1,39 @@
 class Solution {
+
     public int subarraysWithKDistinct(int[] nums, int k) {
-        int n = nums.length;
-        return atmost(nums,k) - atmost(nums,k-1);
-    }
-    public int atmost(int[] nums, int k){
+        
         HashMap<Integer,Integer> mpp = new HashMap<>();
-        int right =0;
         int left =0;
-        int total =0;
+        int right =0;
+        int cnt =0;
         int n = nums.length;
+        int val = k;
+       return helper(nums, k) - helper(nums, k-1);
+    }
+    public int helper(int[] nums, int k){
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        int left =0;
+        int right =0;
+        int cnt =0;
+        int n = nums.length;
+        int val = k;
         while(right < n){
             if(mpp.getOrDefault(nums[right],0) == 0){
                 k--;
             }
-            mpp.put(nums[right], mpp.getOrDefault(nums[right], 0)+1);
+           
+            mpp.put(nums[right], mpp.getOrDefault(nums[right],0)+1);
+          
             while(k < 0){
-                int val = mpp.get(nums[left]);
-                mpp.put(nums[left], val -1);
+                mpp.put(nums[left], mpp.get(nums[left])-1);
                 if(mpp.get(nums[left]) == 0){
                     k++;
                 }
                 left++;
             }
-            total += right - left +1;
+            cnt += (right - left +1);
             right++;
         }
-        return total;
+        return cnt;
     }
 }
