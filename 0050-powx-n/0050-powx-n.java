8ae93453be5c3faf0a44df4a9ca6f1378838d749
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-23 11:50:18 +0530

    Time: 0 ms (100%), Space: 47.6 MB (79.64%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,10 @@
+class Solution {
+    public double myPow(double x, int n) {
+    //   double ans =1;
+    //   for(int i =0;i<n;i++){
+    //     ans *= x;
+    //   } 
+    //   return ans; 
+    return Math.pow(x,n);
+    }
+}
