15ef513c604594e119451edaff11edf8acbd2e19
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-24 15:01:19 +0530

    Time: 44 ms (84.96%), Space: 131.5 MB (46.97%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,61 +1,72 @@
-class Node {
-    int key, value;
+class Node{
     Node prev, next;
-    Node(int key, int value) {
+    int key, value;
+    Node(int key, int value){
         this.key = key;
         this.value = value;
     }
 }
-
 class LRUCache {
     private final int capacity;
-    private final Map<Integer, Node> map;
+    private final HashMap<Integer,Node> mpp;
     private final Node head, tail;
 
     public LRUCache(int capacity) {
-        this.capacity = capacity;
-        this.map = new HashMap<>();
-        head = new Node(0, 0);
-        tail = new Node(0, 0);
+        this.capacity =  capacity;
+        this.mpp = new HashMap<>();
+        head = new Node(0,0);
+        tail = new Node(0,0);  
         head.next = tail;
-        tail.prev = head;
+        tail.prev = head;  
     }
     
     public int get(int key) {
-        if (!map.containsKey(key)) return -1;
-        Node node = map.get(key);
+        if(!mpp.containsKey(key)) return -1;
+        Node node  = mpp.get(key);
+        node.value = node.value;
         remove(node);
-        insertAtFront(node);
+        insertAtfront(node);
         return node.value;
     }
     
     public void put(int key, int value) {
-        if (map.containsKey(key)) {
-            Node node = map.get(key);
-            node.value = value;
-            remove(node);
-            insertAtFront(node);
-        } else {
-            if (map.size() == capacity) {
-                Node lru = tail.prev;
-                remove(lru);
-                map.remove(lru.key);
-            }
-            Node newNode = new Node(key, value);
-            insertAtFront(newNode);
-            map.put(key, newNode);
-        }
-    }
+        if(mpp.containsKey(key)){
+            Node nn = mpp.get(key);
+            nn.value = value;
+            remove(nn);
+            insertAtfront(nn);
 
-    private void remove(Node node) {
+        }
+        else{
+            if(mpp.size() == capacity){
+                 Node lru = tail.prev;
+                 
+                 remove(lru);
+                
+                mpp.remove(lru.key);
+            }
+        
+        Node newN = new Node(key, value);
+        insertAtfront(newN);
+        mpp.put(key,newN);
+        }
+
+    }
+    private void remove(Node node){
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
-
-    private void insertAtFront(Node node) {
+    private void insertAtfront(Node node){
         node.next = head.next;
         node.prev = head;
         head.next.prev = node;
         head.next = node;
     }
 }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
