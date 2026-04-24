f58cdcf2fe983eb7cfa1899279b30499d6e4a252
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-24 23:24:48 +0530

    Time: 22 ms (14.36%), Space: 46.7 MB (48.65%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,28 +1,26 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> res =new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         List<Integer> ans = new ArrayList<>();
+        Set<List<Integer>> st = new HashSet<>();
         for(int a : nums){
             ans.add(a);
         }
-        Set<List<Integer>> st = new HashSet<>();
-        int n = nums.length;
-        helper(st, res, ans, 0,n);
+        helper( res, ans, st, 0);
         return res;
     }
-    public void helper(Set<List<Integer>> st, List<List<Integer>> res, List<Integer> ans, int i, int n){
-        if(i == n-1){
+    public void helper(List<List<Integer>> res, List<Integer> ans, Set<List<Integer>> st, int i){
+        if(i == ans.size()){
             if(!st.contains(ans)){
-            res.add(new ArrayList<>(ans));
-            st.add(ans);
+                res.add(new ArrayList<>(ans));
+                st.add(ans);
+                return;
             }
-            return;
         }
-        for(int k = i; k<n; k++){
+        for(int k =i; k<ans.size(); k++){
             Collections.swap(ans, k,i);
-            helper(st,res,ans, i+1,n);
-            Collections.swap(ans,i,k);
+            helper(res,ans, st, i+1);
+            Collections.swap(ans, i,k);
         }
-
     }
 }
