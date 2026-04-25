b55a1fc3bd846670cfb7049487b70e9ce9520390
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-25 18:46:11 +0530

    Time: 2 ms (99.94%), Space: 45 MB (99.32%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,36 +1,38 @@
 class Solution {
     public String minWindow(String s, String t) {
+        int m = s.length();
+        int n = t.length();
         int[] freq = new int[128];
-        for(char c : t.toCharArray()){
-            freq[c]++;
+        for(char pl : t.toCharArray()){
+            freq[pl]++;
         }
-        int req = t.length();
-        int right = 0;
+        int start = 0;
         int left =0;
         int mini = Integer.MAX_VALUE;
-        int n = s.length();
-        char[] ans = s.toCharArray();
-        int start =0;
-        while(right < n){
-            char val = ans[right];
-            freq[val]--;
-            if(freq[val] >= 0){
+        int req = n;
+        int right = 0;
+        while(right < m){
+            char ok = s.charAt(right);
+            
+            if(freq[ok] > 0){
                 req--;
             }
+            freq[ok]--;
             right++;
             while(req == 0){
-                if((right - left) < mini){
-                    mini = right - left;
+                if(right - left < mini){
                     start = left;
+                    mini = right - left;
                 }
-                char ss = ans[left];
-                freq[ss]++;
-                if(freq[ss] > 0){
+
+                char pl = s.charAt(left);
+                freq[pl]++;
+                if(freq[pl] > 0){
                     req++;
                 }
                 left++;
             }
         }
-        return mini == Integer.MAX_VALUE ? "" : s.substring(start, start + mini);
+        return (mini == Integer.MAX_VALUE) ?  "" : s.substring(start, start + mini);
     }
 }
