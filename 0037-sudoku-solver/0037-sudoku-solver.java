69043250157b91e08ddf67040bb0f6d057a41b0d
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-25 20:11:59 +0530

    Time: 171 ms (77.87%), Space: 43 MB (19.04%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -4,18 +4,18 @@ class Solution {
     }
     public boolean helper(char[][] board, int i, int j){
         if(j == 9){
-           return helper(board, i+1, 0);
+            return helper(board, i+1, 0);
         }
         if(i == 9){
             return true;
         }
         if(board[i][j] != '.'){
-            return helper(board, i,j+1);
+            return helper(board,i,j+1);
         }
-        for(char k ='1'; k<='9'; k++){
+        for(char k = '1'; k<='9'; k++){
             if(isSafe(board, i,j,k)){
                 board[i][j] = k;
-                if(helper(board, i,j+1)){
+                if(helper(board,i,j+1)){
                     return true;
                 }
                 board[i][j] = '.';
@@ -24,8 +24,8 @@ class Solution {
         return false;
     }
     public boolean isSafe(char[][] board, int row, int col, char key){
-        for(int k = 0; k<9; k++){
-            if(board[row][k] == key){
+        for(int i =0; i<9; i++){
+            if(board[row][i] == key){
                 return false;
             }
         }
@@ -38,13 +38,13 @@ class Solution {
         int startR = (row/3) * 3;
         int startC = (col/3) * 3;
 
-        for(int i =startR; i<startR + 3; i++){
-            for(int j =startC; j<startC+3; j++){
+        for(int i = startR; i<startR + 3; i++){
+            for(int j = startC; j<startC+ 3; j++){
                 if(board[i][j] == key){
                     return false;
                 }
             }
         }
         return true;
-    } 
+    }
 }
