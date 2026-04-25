bd69efaa24aac3b4d6385ee599bdea23d497fbcc
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-26 04:39:45 +0530

    Time: 1 ms (99.53%), Space: 44.9 MB (88.15%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -13,41 +13,43 @@
  *     }
  * }
  */
-class Solution {
-    class Pair{
-        TreeNode ab;
-        int ac;
-        Pair(TreeNode ab, int ac){
-            this.ab = ab;
-            this.ac = ac;
-        }
+
+class Pair{
+    TreeNode nbr;
+    int val;
+    Pair(TreeNode nbr, int val){
+        this.nbr = nbr;
+        this.val = val;
     }
+} 
+class Solution {
     public int widthOfBinaryTree(TreeNode root) {
         Queue<Pair> q = new ArrayDeque<>();
-        q.offer(new Pair(root,0));
-        int maxi =0;
-        int start =0;
-        int end =0;
+        q.offer(new Pair(root, 0));
+        int maxi  = Integer.MIN_VALUE;
+        int start = -1;
+        int end = -1;
         while(!q.isEmpty()){
             int ss = q.size();
             for(int i =0; i<ss; i++){
-                Pair nbr = q.poll();
-                TreeNode node = nbr.ab;
-                int idx = nbr.ac;
+                Pair nn = q.poll();
+                TreeNode a = nn.nbr;
+                int b = nn.val;
                 if(i == 0){
-                    start = idx;
+                    start = b;
                 }
-                if(i == ss-1){
-                    end = idx;
+                if(i == ss -1){
+                    end= b;
                 }
-                if(node.left != null){
-                    q.offer(new Pair(node.left, 2 * idx+1));
+                if(a.left != null){
+                    q.offer(new Pair(a.left, 2 * b+1));
                 }
-                if(node.right != null){
-                    q.offer(new Pair(node.right, 2 * idx+2));
+                if(a.right != null){
+                    q.offer(new Pair(a.right, 2 * b+2));
                 }
+                
             }
-            maxi = Math.max(maxi, end-start+1);
+            maxi = Math.max(maxi, end - start + 1);
         }
         return maxi;
     }
