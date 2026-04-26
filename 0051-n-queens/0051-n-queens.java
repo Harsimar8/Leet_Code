bde6992805562f8175370d2445e928ec52d50b9f
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-26 12:33:38 +0530

    Time: 2 ms (81.88%), Space: 46.7 MB (29.03%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,54 +1,51 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        char[][] board = new char[n][n];
-        for(int i =0; i<n;i++){
-            Arrays.fill(board[i],'.');
-        }
         List<List<String>> res = new ArrayList<>();
         int row = 0;
-        return nQueen(board, res, row, n);
+        char[][] board = new char[n][n];
+        for(char[] ab : board){
+            Arrays.fill(ab , '.');
+        }
+        helper(board,res, row, n);
+        return res;
+    }
+    public void helper(char[][] board, List<List<String>> res, int row , int n){
+        if(row == n){
+            res.add(convert(board));
+        }
+        for(int j =0; j<n; j++){
+            if(isSafe(board, row, j, n)){
+                board[row][j] = 'Q';
+                helper(board, res, row+1, n);
+                board[row][j] = '.';
+            }
+        }
+        
+    }
+    public List<String> convert(char[][] board){
+        List<String> ans = new ArrayList<>();
+        for(char[] row : board){
+            ans.add(new String(row));
+        }
+        return ans;
     }
     public boolean isSafe(char[][] board, int row, int col, int n){
-        for(int j = 0; j<row; j++){
-            if(board[j][col] == 'Q'){
+        for(int i =0; i<row; i++){
+            if(board[i][col] == 'Q'){
                 return false;
             }
         }
-        for(int i =row-1,  j=col-1; i>=0 && j>=0 ; i--,j--){
+        for(int i =row-1,j = col-1; i>=0 && j>=0 ;i--,j--){
             if(board[i][j] == 'Q'){
                 return false;
             }
         }
-        for(int i =row-1, j= col+1 ; i>=0 && j<n ; i--,j++){
+
+        for(int i =row-1,j = col+1; i>=0 && j<n ;i--,j++){
             if(board[i][j] == 'Q'){
                 return false;
             }
         }
         return true;
-
     }
-    public   List<List<String>> nQueen(char[][] board,List<List<String>> ans,int row, int n){
-        if(row == n){
-            ans.add(convert(board));
-            
-        }
-        for(int j =0; j<n;j++){
-            if(isSafe(board, row, j, n)){
-                board[row][j] = 'Q';
-                nQueen(board, ans, row +1, n);
-                board[row][j] ='.';
-            }
-            
-        }
-        return ans; 
-    }
-
-    public List<String> convert(char[][] board){
-        List<String> res = new ArrayList<>();
-        for(char[] row : board){
-            res.add(new String(row));
-        }
-        return res;
-    }
-
 }
