3ea8134c5d19be3c349f5cef0d7373cfc884cec1
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-04-26 20:09:16 +0530

    Time: 42 ms (86.24%), Space: 64.3 MB (49.36%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,33 +1,34 @@
 class Solution {
     public int reversePairs(int[] nums) {
         int n = nums.length;
-        return merge(nums,0,n-1);
+        return merge(nums, 0,n-1);
+
     }
-    public int merge(int[] nums, int low, int high){
+    public int merge(int[] nums, int low,int high){
         if(low >= high){
             return 0;
         }
-        int mid = low + (high - low)/2;
+        int mid = (low + high)/2;
         int left = merge(nums, low, mid);
         int right = merge(nums, mid+1, high);
-        int finalC = mergeSort(nums, low, mid, high);
-        return left + right + finalC;
+        int finalM = mergeSort(nums, low, mid, high);
+        return left + right + finalM;
     }
-    public int mergeSort(int[] nums, int low, int mid , int high){
-        int j = mid +1;
+    public int mergeSort(int[] nums, int low, int mid, int high){
+        int j = mid + 1;
         int mm = j;
-        int right = mid;
+        int[] temp = new int[high - low + 1];
         int left = low;
-        int[] temp = new int[high - low +1];
-        int last =0;
-        for(int i =low; i<=mid; i++){
-            while(j <= high && (long) nums[i] > (long)2 * nums[j]){
+        int last = 0;
+        int k =0;
+        int right = mid;
+        for(int i=low; i<=mid; i++){
+            while(j <= high && (long)nums[i] > (long)2*nums[j]){
                 j++;
             }
-            last += (j - (mid+1));
+            last += (j- (mid+1));
         }
-
-        int k =0;
+        
         while(left <= mid && mm <= high){
             if(nums[left] < nums[mm]){
                 temp[k] = nums[left];
@@ -35,23 +36,24 @@ class Solution {
                 k++;
             }
             else{
-                 temp[k] = nums[mm];
+                temp[k] = nums[mm];
                 mm++;
                 k++;
             }
         }
         while(left <= mid){
-            temp[k] = nums[left];
+                temp[k] = nums[left];
                 left++;
                 k++;
+            
         }
         while(mm <= high){
-              temp[k] = nums[mm];
-                mm++;
-                k++;
+            temp[k] = nums[mm];
+            mm++;
+            k++;
         }
         for(int l =0; l<temp.length; l++){
-            nums[low+l] = temp[l];
+            nums[low + l] = temp[l];
         }
         return last;
     }
