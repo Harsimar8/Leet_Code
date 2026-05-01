408146fcfd7d576348224c0d50c6c5928cef9148
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-05-01 18:55:12 +0530

    Time: 24 ms (40.94%), Space: 53.9 MB (62.17%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -1,14 +1,15 @@
 class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
         int m = str1.length();
-        int n = str2.length(); 
-        int i =m;
+        int n = str2.length();
+        int i = m;
         int j = n;
         int[][] dp = new int[m+1][n+1];
-        for(int r = 1; r <=m; r++){
-            for(int s = 1; s <=n; s++){
+
+        for(int r = 1; r<=m; r++){
+            for(int s = 1; s<=n; s++){
                 if(str1.charAt(r-1) == str2.charAt(s-1)){
-                    dp[r][s]= 1 + dp[r-1][s-1]; 
+                    dp[r][s] = 1 + dp[r-1][s-1];
                 }
                 else{
                     dp[r][s] = Math.max(dp[r-1][s], dp[r][s-1]);
@@ -16,7 +17,7 @@ class Solution {
             }
         }
         StringBuilder sb = new StringBuilder();
-        while( i > 0 && j > 0){
+        while(i > 0 && j > 0){
             if(str1.charAt(i-1) == str2.charAt(j-1)){
                 sb.append(str1.charAt(i-1));
                 i--;
@@ -29,16 +30,20 @@ class Solution {
             else{
                 sb.append(str2.charAt(j-1));
                 j--;
+                
             }
+ 
+           
         }
-        while( i > 0){
-            sb.append(str1.charAt(i-1));
-            i--;
+        while(i > 0){
+             sb.append(str1.charAt(i-1));
+             i--;
         }
-        while(j > 0){
-            sb.append(str2.charAt(j-1));
-                j--;
+         while(j > 0){
+             sb.append(str2.charAt(j-1));
+             j--;
         }
         return sb.reverse().toString();
+        
     }
 }
