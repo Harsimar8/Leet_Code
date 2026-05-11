8a502a7aa639d12a3af1e6adde1caa0e3331dfe7
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-05-12 02:16:21 +0530

    Time: 1 ms (59%), Space: 48.6 MB (28.05%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,21 +1,21 @@
 class Solution {
-    public int trap(int[] height) {
-        int n = height.length;
-        int[] leftM = new int[n];
-        int[] rightM = new int[n];
-        leftM[0] = height[0];
+    public int trap(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right= new int[n];
+        left[0] = nums[0];
+        right[n-1] = nums[n-1];
+
         for(int i =1; i<n; i++){
-            leftM[i] = Math.max(height[i], leftM[i-1]);
+            left[i] = Math.max(left[i-1], nums[i]);
         }
-        rightM[n-1] = height[n-1];
-        for(int j = n-2; j>=0; j--){
-            rightM[j] = Math.max(height[j], rightM[j+1]);
+        for(int k = n-2; k>=0; k--){
+            right[k] = Math.max(right[k+1], nums[k]);
         }
-        int total =0;
-        for(int k =1; k<n-1; k++){
-            int val = Math.min(leftM[k], rightM[k]);
-            int car = Math.abs(height[k] - val);
-            total += car;
+        int total = 0;
+        for(int j = 1; j<=n-2; j++){
+            int val = Math.min(left[j], right[j]);
+            total += val - nums[j];
         }
         return total;
     }
