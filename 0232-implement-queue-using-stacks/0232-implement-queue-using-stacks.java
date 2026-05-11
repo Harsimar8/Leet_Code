c050fd6c8148af0be8b8eae80eb74dbea07a93b8
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2026-05-12 02:56:52 +0530

    Time: 0 ms (100%), Space: 42.9 MB (20.61%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,46 +1,45 @@
 class MyQueue {
-
-    int[] arr;
-    int front;
-    int rear;
-    int size;
-
+    private int[] arr;
+    private int size = 1000;
+    private int front;
+    private int rear;
     public MyQueue() {
-        size = 1000;
         arr = new int[size];
         front = 0;
         rear = -1;
     }
     
     public void push(int x) {
-
-        if(rear == size - 1){
+        if(rear == size -1){
             return;
         }
-
         arr[++rear] = x;
     }
     
     public int pop() {
-
         if(empty()){
             return -1;
         }
-
         return arr[front++];
     }
     
     public int peek() {
-
         if(empty()){
             return -1;
         }
-
         return arr[front];
     }
     
     public boolean empty() {
-
-        return front > rear;
+        return (front > rear);
     }
 }
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
